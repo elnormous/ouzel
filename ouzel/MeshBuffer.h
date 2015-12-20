58ba53e9aafcb3042d9fa7c973a40257800a0ed5
Elviss Strazdins <elviss@elviss.lv>
2015-12-20 16:11:41 +0200

    Add classes for ParticleSystem and MeshBuffer

---
+++ ouzel/MeshBuffer.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2015 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#pragma once
+
+#include "Noncopyable.h"
+#include "ReferenceCounted.h"
+
+namespace ouzel
+{
+    class MeshBuffer: public Noncopyable, public ReferenceCounted
+    {
+        
+    };
+}
---
+++ ouzel/ParticleSystem.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2015 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#pragma once
+
+#include "node.h"
+
+namespace ouzel
+{
+    class ParticleSystem: public Node
+    {
+        
+    };
+}
