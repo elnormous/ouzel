237735f4b2b3faac92e8dc07a1bf2fb2b96d8f00
Elviss Strazdins <elviss@elviss.lv>
2015-12-21 03:02:01 +0200

    Add classes for sound

---
+++ ouzel/Sound.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2015 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#include "Sound.h"
+
+namespace ouzel
+{
+    Sound::Sound()
+    {
+        
+    }
+    
+    Sound::~Sound()
+    {
+        
+    }
+}
---
+++ ouzel/SoundManager.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2015 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#include "SoundManager.h"
+
+namespace ouzel
+{
+    SoundManager::SoundManager()
+    {
+        
+    }
+    
+    SoundManager::~SoundManager()
+    {
+        
+    }
+}
