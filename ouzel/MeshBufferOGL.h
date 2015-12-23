f2a27ee64f85927dc5bcb8161e8cad6bf441f148
Elviss Strazdins <elviss@elviss.lv>
2015-12-23 04:59:23 +0200

    Fix OpenGLES includes

--- ouzel/MeshBufferOGL.h
+++ ouzel/MeshBufferOGL.h
@@ -5,8 +5,16 @@
 
 #include "CompileConfig.h"
 
-#ifdef OUZEL_PLATFORM_OSX
+#if defined(OUZEL_PLATFORM_OSX)
 #include <OpenGL/gl3.h>
+#elif defined(OUZEL_PLATFORM_IOS)
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+
+#define glBindVertexArray glBindVertexArrayOES
+#define glGenVertexArrays glGenVertexArraysOES
+#define glDeleteVertexArrays glDeleteVertexArraysOES
+
 #endif
 
 #include "MeshBuffer.h"
