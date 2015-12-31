bff7761aeab2a6ead89d216e66ae8d30cc16f523
Elviss Strazdins <elviss@elviss.lv>
2015-12-31 14:48:53 +0200

    Add tvOS target Add Root View Controller for iOS window

---
+++ ouzel/ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ouzel
+//
+//  Created by Elviss Strazdins on 31/12/15.
+//  Copyright © 2015 Bool Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ouzel/ios/ViewController.m
@@ -0,0 +1,13 @@
+//
+//  ViewController.m
+//  ouzel
+//
+//  Created by Elviss Strazdins on 31/12/15.
+//  Copyright © 2015 Bool Games. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@end
