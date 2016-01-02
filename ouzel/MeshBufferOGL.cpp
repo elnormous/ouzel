d9caf7227ba318c0ea47bfeb21f4f5bf1d484e79
Elviss Strazdins <elviss@elviss.lv>
2016-01-02 04:04:00 +0200

    Unbind OpenGL vertex and index buffers after initialization

--- ouzel/MeshBufferOGL.cpp
+++ ouzel/MeshBufferOGL.cpp
@@ -64,6 +64,9 @@ namespace ouzel
             return false;
         }
         
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        
         _indexCount = static_cast<GLsizei>(indices.size());
         
         return true;
