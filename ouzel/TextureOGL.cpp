3ffd65baaa722a1fa76948e8983b4e396008c17b
Elviss Strazdins <elviss@elviss.lv>
2016-01-14 10:45:18 +0200

    Fix OpenGL texture data uploading

--- ouzel/TextureOGL.cpp
+++ ouzel/TextureOGL.cpp
@@ -56,6 +56,8 @@ namespace ouzel
     
     bool TextureOGL::uploadData(const void* data, GLsizei width, GLsizei height)
     {
+        glBindTexture(GL_TEXTURE_2D, _textureId);
+        
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height,
                      0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         
