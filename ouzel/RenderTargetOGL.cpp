4cec4351ef2774ed29178d25b9c9b0aead512aa3
Elviss Strazdins <elviss@elviss.lv>
2016-01-16 15:24:14 +0200

    Start implementing OpenGL render target

--- ouzel/RenderTargetOGL.cpp
+++ ouzel/RenderTargetOGL.cpp
@@ -14,4 +14,16 @@ namespace ouzel
     {
         
     }
+    
+    bool RenderTargetOGL::init(Size2 const& size)
+    {
+        glGenFramebuffers(1, &_framebufferId);
+        glBindFramebuffer(GL_FRAMEBUFFER, _framebufferId);
+        
+        glBindTexture(GL_TEXTURE_2D, _textureId);
+        
+        glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, size.width, size.height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+        
+        return true;
+    }
 }
--- ouzel/RenderTargetOGL.h
+++ ouzel/RenderTargetOGL.h
@@ -3,7 +3,25 @@
 
 #pragma once
 
+#include "CompileConfig.h"
 #include "RenderTarget.h"
+#include "Size2.h"
+
+#if defined(OUZEL_PLATFORM_OSX)
+#include <OpenGL/gl3.h>
+#elif defined(OUZEL_PLATFORM_IOS) || defined(OUZEL_PLATFORM_TVOS)
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#elif defined(OUZEL_PLATFORM_ANDROID)
+#include <GLES2/gl2platform.h>
+#ifndef GL_GLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES 1
+#endif
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <EGL/egl.h>
+#endif
 
 namespace ouzel
 {
@@ -15,7 +33,15 @@ namespace ouzel
     public:
         virtual ~RenderTargetOGL();
         
+        bool init(Size2 const& size);
+        
+        GLuint getFrameBufferId() const { return _framebufferId; }
+        GLuint getTextureId() const { return _textureId; }
+        
     protected:
         RenderTargetOGL();
+        
+        GLuint _framebufferId;
+        GLuint _textureId;
     };
 }
