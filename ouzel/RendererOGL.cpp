3b758783a9730bd17285cff032fc44976a0ab918
Elviss Strazdins <elviss@elviss.lv>
2016-01-25 03:09:44 +0200

    Set blend functions and factors separately for OpenGL

--- ouzel/RendererOGL.cpp
+++ ouzel/RendererOGL.cpp
@@ -56,10 +56,8 @@ namespace ouzel
         glClearColor(_clearColor.getR(), _clearColor.getG(), _clearColor.getB(), _clearColor.getA());
         
         glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        
-        //glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
-        //glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
+        glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
+        glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
         
         // precomputed alpha
         //glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
