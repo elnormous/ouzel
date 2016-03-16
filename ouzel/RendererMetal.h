c5711d6dc4b7c5f2971ddc705105eb166e1b607e
Elviss Strazdins <elviss@elviss.lv>
2016-03-16 23:16:17 +0200

    Moved Metal renderer to video namespace

--- ouzel/RendererMetal.h
+++ ouzel/RendererMetal.h
@@ -6,30 +6,33 @@
 #include "Renderer.h"
 
 namespace ouzel
-{    
-    class RendererMetal: public Renderer
+{
+    namespace video
     {
-        friend Engine;
-    public:
-        virtual ~RendererMetal();
-        
-        virtual void setClearColor(Color color) override;
-        
-        virtual void setSize(const Size2& size) override;
-        
-        virtual void clear() override;
-        virtual void flush() override;
-        
-        virtual std::shared_ptr<Texture> loadTextureFromFile(const std::string& filename, bool dynamic, bool mipmaps = true) override;
-        virtual std::shared_ptr<Texture> loadTextureFromData(const void* data, const Size2& size, bool dynamic, bool mipmaps = true) override;
-        
-        virtual std::shared_ptr<Shader> loadShaderFromFiles(const std::string& fragmentShader, const std::string& vertexShader, uint32_t vertexAttributes) override;
-        virtual std::shared_ptr<Shader> loadShaderFromBuffers(const uint8_t* fragmentShader, uint32_t fragmentShaderSize, const uint8_t* vertexShader, uint32_t vertexShaderSize, uint32_t vertexAttributes) override;
-        
-        virtual std::shared_ptr<MeshBuffer> createMeshBuffer(const void* indices, uint32_t indexSize, uint32_t indexCount, bool dynamicIndexBuffer, const void* vertices, uint32_t vertexSize, uint32_t vertexCount, bool dynamicVertexBuffer, uint32_t vertexAttributes) override;
-        virtual bool drawMeshBuffer(const MeshBufferPtr& meshBuffer, uint32_t indexCount = 0, DrawMode drawMode = DrawMode::TRIANGLE_LIST) override;
-        
-    private:
-        RendererMetal();
-    };
-}
+        class RendererMetal: public Renderer
+        {
+            friend Engine;
+        public:
+            virtual ~RendererMetal();
+            
+            virtual void setClearColor(Color color) override;
+            
+            virtual void setSize(const Size2& size) override;
+            
+            virtual void clear() override;
+            virtual void flush() override;
+            
+            virtual std::shared_ptr<Texture> loadTextureFromFile(const std::string& filename, bool dynamic, bool mipmaps = true) override;
+            virtual std::shared_ptr<Texture> loadTextureFromData(const void* data, const Size2& size, bool dynamic, bool mipmaps = true) override;
+            
+            virtual std::shared_ptr<Shader> loadShaderFromFiles(const std::string& fragmentShader, const std::string& vertexShader, uint32_t vertexAttributes) override;
+            virtual std::shared_ptr<Shader> loadShaderFromBuffers(const uint8_t* fragmentShader, uint32_t fragmentShaderSize, const uint8_t* vertexShader, uint32_t vertexShaderSize, uint32_t vertexAttributes) override;
+            
+            virtual std::shared_ptr<MeshBuffer> createMeshBuffer(const void* indices, uint32_t indexSize, uint32_t indexCount, bool dynamicIndexBuffer, const void* vertices, uint32_t vertexSize, uint32_t vertexCount, bool dynamicVertexBuffer, uint32_t vertexAttributes) override;
+            virtual bool drawMeshBuffer(const MeshBufferPtr& meshBuffer, uint32_t indexCount = 0, DrawMode drawMode = DrawMode::TRIANGLE_LIST) override;
+            
+        private:
+            RendererMetal();
+        };
+    } // namespace video
+} // namespace ouzel
--- ouzel/RendererMetal.mm
+++ ouzel/RendererMetal.mm
@@ -5,103 +5,106 @@
 
 namespace ouzel
 {
-    RendererMetal::RendererMetal()
+    namespace video
     {
+        RendererMetal::RendererMetal()
+        {
+            
+        }
         
-    }
-    
-    RendererMetal::~RendererMetal()
-    {
+        RendererMetal::~RendererMetal()
+        {
+            
+        }
         
-    }
-    
-    void RendererMetal::setClearColor(Color color)
-    {
+        void RendererMetal::setClearColor(Color color)
+        {
 
-    }
+        }
 
-    void RendererMetal::setSize(const Size2& size)
-    {
+        void RendererMetal::setSize(const Size2& size)
+        {
 
-    }
-    
-    void RendererMetal::clear()
-    {
+        }
+        
+        void RendererMetal::clear()
+        {
 
-    }
-    
-    void RendererMetal::flush()
-    {
+        }
+        
+        void RendererMetal::flush()
+        {
 
-    }
-    
-    std::shared_ptr<Texture> RendererMetal::loadTextureFromFile(const std::string& filename, bool dynamic, bool mipmaps)
-    {
-        std::shared_ptr<TextureMetal> texture(new TextureMetal());
-        
-        if (!texture->initFromFile(filename, dynamic, mipmaps))
-        {
-            texture.reset();
         }
         
-        return texture;
-    }
-    
-    std::shared_ptr<Texture> RendererMetal::loadTextureFromData(const void* data, const Size2& size, bool dynamic, bool mipmaps)
-    {
-        std::shared_ptr<TextureMetal> texture(new TextureMetal());
-        
-        if (!texture->initFromData(data, size, dynamic, mipmaps))
+        std::shared_ptr<Texture> RendererMetal::loadTextureFromFile(const std::string& filename, bool dynamic, bool mipmaps)
         {
-            texture.reset();
+            std::shared_ptr<TextureMetal> texture(new TextureMetal());
+            
+            if (!texture->initFromFile(filename, dynamic, mipmaps))
+            {
+                texture.reset();
+            }
+            
+            return texture;
         }
         
-        return texture;
-    }
-    
-    std::shared_ptr<Shader> RendererMetal::loadShaderFromFiles(const std::string& fragmentShader, const std::string& vertexShader, uint32_t vertexAttributes)
-    {
-        std::shared_ptr<ShaderMetal> shader(new ShaderMetal());
-        
-        if (!shader->initFromFiles(fragmentShader, vertexShader, vertexAttributes))
+        std::shared_ptr<Texture> RendererMetal::loadTextureFromData(const void* data, const Size2& size, bool dynamic, bool mipmaps)
         {
-            shader.reset();
+            std::shared_ptr<TextureMetal> texture(new TextureMetal());
+            
+            if (!texture->initFromData(data, size, dynamic, mipmaps))
+            {
+                texture.reset();
+            }
+            
+            return texture;
         }
         
-        return shader;
-    }
-    
-    std::shared_ptr<Shader> RendererMetal::loadShaderFromBuffers(const uint8_t* fragmentShader, uint32_t fragmentShaderSize, const uint8_t* vertexShader, uint32_t vertexShaderSize, uint32_t vertexAttributes)
-    {
-        std::shared_ptr<ShaderMetal> shader(new ShaderMetal());
-        
-        if (!shader->initFromBuffers(fragmentShader, fragmentShaderSize, vertexShader, vertexShaderSize, vertexAttributes))
+        std::shared_ptr<Shader> RendererMetal::loadShaderFromFiles(const std::string& fragmentShader, const std::string& vertexShader, uint32_t vertexAttributes)
         {
-            shader.reset();
+            std::shared_ptr<ShaderMetal> shader(new ShaderMetal());
+            
+            if (!shader->initFromFiles(fragmentShader, vertexShader, vertexAttributes))
+            {
+                shader.reset();
+            }
+            
+            return shader;
         }
         
-        return shader;
-    }
-    
-    std::shared_ptr<MeshBuffer> RendererMetal::createMeshBuffer(const void* indices, uint32_t indexSize, uint32_t indexCount, bool dynamicIndexBuffer, const void* vertices, uint32_t vertexSize, uint32_t vertexCount, bool dynamicVertexBuffer, uint32_t vertexAttributes)
-    {
-        std::shared_ptr<MeshBufferMetal> meshBuffer(new MeshBufferMetal());
-        
-        if (!meshBuffer->initFromData(indices, indexSize, indexCount, dynamicIndexBuffer, vertices, vertexSize, vertexCount, dynamicVertexBuffer, vertexAttributes))
+        std::shared_ptr<Shader> RendererMetal::loadShaderFromBuffers(const uint8_t* fragmentShader, uint32_t fragmentShaderSize, const uint8_t* vertexShader, uint32_t vertexShaderSize, uint32_t vertexAttributes)
         {
-            meshBuffer.reset();
+            std::shared_ptr<ShaderMetal> shader(new ShaderMetal());
+            
+            if (!shader->initFromBuffers(fragmentShader, fragmentShaderSize, vertexShader, vertexShaderSize, vertexAttributes))
+            {
+                shader.reset();
+            }
+            
+            return shader;
         }
         
-        return meshBuffer;
-    }
-    
-    bool RendererMetal::drawMeshBuffer(const MeshBufferPtr& meshBuffer, uint32_t indexCount, DrawMode drawMode)
-    {
-        if (!Renderer::drawMeshBuffer(meshBuffer, indexCount, drawMode))
+        std::shared_ptr<MeshBuffer> RendererMetal::createMeshBuffer(const void* indices, uint32_t indexSize, uint32_t indexCount, bool dynamicIndexBuffer, const void* vertices, uint32_t vertexSize, uint32_t vertexCount, bool dynamicVertexBuffer, uint32_t vertexAttributes)
         {
-            return false;
+            std::shared_ptr<MeshBufferMetal> meshBuffer(new MeshBufferMetal());
+            
+            if (!meshBuffer->initFromData(indices, indexSize, indexCount, dynamicIndexBuffer, vertices, vertexSize, vertexCount, dynamicVertexBuffer, vertexAttributes))
+            {
+                meshBuffer.reset();
+            }
+            
+            return meshBuffer;
         }
         
-        return true;
-    }
-}
+        bool RendererMetal::drawMeshBuffer(const MeshBufferPtr& meshBuffer, uint32_t indexCount, DrawMode drawMode)
+        {
+            if (!Renderer::drawMeshBuffer(meshBuffer, indexCount, drawMode))
+            {
+                return false;
+            }
+            
+            return true;
+        }
+    } // namespace video
+} // namespace ouzel
