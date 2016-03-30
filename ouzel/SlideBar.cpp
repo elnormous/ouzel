f6003f9bd9a742b0ef0d155cf6e3d5bae2caecfb
Elviss Strazdins <elviss@elviss.lv>
2016-03-30 20:06:25 +0300

    Add SlideBar class

---
+++ ouzel/SlideBar.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2016 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#include "SlideBar.h"
+
+namespace ouzel
+{
+    namespace gui
+    {
+
+    } // namespace gui
+} // namespace ouzel
