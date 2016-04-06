afe440bd2844cc145473df3b89885672f812894e
Elviss Strazdins <elviss@elviss.lv>
2016-04-07 01:00:38 +0300

    Rename BlendStateMetal.cpp to BlendStateMetal.mm

--- ouzel/metal/BlendStateMetal.h
+++ ouzel/metal/BlendStateMetal.h
@@ -3,16 +3,35 @@
 
 #pragma once
 
+#include "CompileConfig.h"
 #include "BlendState.h"
+
+#ifdef __OBJC__
+#import <Metal/Metal.h>
+typedef MTLRenderPipelineColorAttachmentDescriptor* MTLRenderPipelineColorAttachmentDescriptorPtr;
+#else
+#include <objc/objc.h>
+typedef id MTLRenderPipelineColorAttachmentDescriptorPtr;
+#endif
 
 namespace ouzel
 {
     namespace video
     {
+        class MetalRenderer;
+
         class BlendStateMetal: public BlendState
         {
+            friend MetalRenderer;
         public:
-            virtual ~BlendStateMetal() {}
+            virtual ~BlendStateMetal();
+
+        protected:
+            BlendStateMetal();
+
+            void destroy();
+
+            MTLRenderPipelineColorAttachmentDescriptorPtr _blendState = Nil;
         };
     } // namespace video
 } // namespace ouzel
