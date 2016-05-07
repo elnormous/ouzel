687413448c53ca98e1e7f6c765aeb0c2f1f3c316
Elviss Strazdins <elviss@elviss.lv>
2016-05-07 15:06:05 -0700

    Log D3D11 render target view creation failure

--- ouzel/direct3d11/RenderTargetD3D11.cpp
+++ ouzel/direct3d11/RenderTargetD3D11.cpp
@@ -5,6 +5,7 @@
 #include "TextureD3D11.h"
 #include "Engine.h"
 #include "RendererD3D11.h"
+#include "Utils.h"
 
 namespace ouzel
 {
@@ -50,13 +51,14 @@ namespace ouzel
             std::shared_ptr<RendererD3D11> rendererD3D11 = std::static_pointer_cast<RendererD3D11>(sharedEngine->getRenderer());
 
             D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
-            renderTargetViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // TODO: use this in D3D11Texture
+            renderTargetViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
             renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
             renderTargetViewDesc.Texture2D.MipSlice = 0;
 
             HRESULT hr = rendererD3D11->getDevice()->CreateRenderTargetView(textureD3D11->getTexture(), &renderTargetViewDesc, &renderTargetView);
             if (FAILED(hr))
             {
+				log("Failed to create D3D11 render target view");
                 return false;
             }
 
