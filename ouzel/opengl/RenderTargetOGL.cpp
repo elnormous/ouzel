f1aafce2d6db94d32c3f1e03f64e99f15e76c48d
Elviss Strazdins <elviss@elviss.lv>
2016-05-10 12:25:00 +0300

    Fix OpenGL render textuer pixel format

--- ouzel/opengl/RenderTargetOGL.cpp
+++ ouzel/opengl/RenderTargetOGL.cpp
@@ -69,7 +69,7 @@ namespace ouzel
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
                          static_cast<GLsizei>(size.width),
                          static_cast<GLsizei>(size.height),
-                         0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+                         0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
