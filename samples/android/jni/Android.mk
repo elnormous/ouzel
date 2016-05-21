b3f1d5472f84af9854a74add6a68b6aaad723116
Elviss Strazdins <elviss@elviss.lv>
2016-05-21 21:30:26 +0300

    Use local path in samples' Android makefile

--- samples/android/jni/Android.mk
+++ samples/android/jni/Android.mk
@@ -5,17 +5,17 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := samples
 LOCAL_CFLAGS := -Wall -Wextra
 LOCAL_CPPFLAGS += -std=c++11
-LOCAL_C_INCLUDES += ../../../ouzel
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../ouzel
 
 LOCAL_PCH := Prefix.h
 
-LOCAL_SRC_FILES := jni.cpp \
-    ../../Application.cpp \
-    ../../main.cpp
+LOCAL_SRC_FILES := $(LOCAL_PATH)/jni.cpp \
+    $(LOCAL_PATH)/../../Application.cpp \
+    $(LOCAL_PATH)/../../main.cpp
 
 LOCAL_STATIC_LIBRARIES := ouzel
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lEGL -llog
 
 include $(BUILD_SHARED_LIBRARY)
 
-include ../../../build/jni/Android.mk
+include $(LOCAL_PATH)/../../../build/jni/Android.mk
