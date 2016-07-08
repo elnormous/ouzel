778c54c4e7130230d3cadd6349abd8ff5ab05db4
Elviss Strazdins <elviss@elviss.lv>
2016-07-09 01:20:00 +0300

    Check if sourceId is valid

--- ouzel/openal/SoundAL.cpp
+++ ouzel/openal/SoundAL.cpp
@@ -19,7 +19,10 @@ namespace ouzel
         {
             if (outputBuffer)
             {
-                alSourceUnqueueBuffers(sourceId, 1, &outputBuffer);
+                if (sourceId)
+                {
+                    alSourceUnqueueBuffers(sourceId, 1, &outputBuffer);
+                }
 
                 alDeleteBuffers(1, &outputBuffer);
 
