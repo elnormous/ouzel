bdf7509928f77bc75be5a3f17353000c6b743d13
Elviss Strazdins <elviss@elviss.lv>
2016-08-05 19:59:03 +0300

    Add -sample and -renderer argument to samples

--- samples/main.cpp
+++ samples/main.cpp
@@ -8,17 +8,58 @@ Samples samples;
 
 void ouzelMain(const std::vector<std::string>& args)
 {
-    std::string sample;
-
-    if (args.size() >= 2)
-    {
-        sample = args[1];
-    }
-
     ouzel::Settings settings;
     settings.size = ouzel::Size2(800.0f, 600.0f);
     settings.resizable = true;
     settings.sampleCount = 4;
     settings.textureFiltering = ouzel::graphics::Renderer::TextureFiltering::TRILINEAR;
+
+    std::string sample;
+
+    for (auto arg = args.begin() + 1; arg != args.end(); ++arg)
+    {
+        if (*arg == "-sample")
+        {
+            auto nextArg = ++arg;
+
+            if (nextArg != args.end())
+            {
+                sample = *nextArg;
+            }
+            else
+            {
+                ouzel::log("No sample specified");
+            }
+        }
+        else if (*arg == "-renderer")
+        {
+            auto nextArg = ++arg;
+
+            if (nextArg != args.end())
+            {
+                if (*nextArg == "opengl")
+                {
+                    settings.driver = ouzel::graphics::Renderer::Driver::OPENGL;
+                }
+                else if (*nextArg == "direct3d11")
+                {
+                    settings.driver = ouzel::graphics::Renderer::Driver::DIRECT3D11;
+                }
+                else if (*nextArg == "metal")
+                {
+                    settings.driver = ouzel::graphics::Renderer::Driver::METAL;
+                }
+            }
+            else
+            {
+                ouzel::log("No renderer specified");
+            }
+        }
+        else
+        {
+            ouzel::log("Invalid argument \"%s\"", arg->c_str());
+        }
+    }
+
     engine.init(settings, std::bind(&Samples::begin, &samples, sample));
 }
