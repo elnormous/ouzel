0f92c70368cc9ba3fede90f86f999c1edadcf50d
elnormous <elviss@elviss.lv>
2016-08-29 23:21:46 +0300

    Remove unneded OpenGL filter

--- ouzel/opengl/TextureOGL.cpp
+++ ouzel/opengl/TextureOGL.cpp
@@ -55,9 +55,7 @@ namespace ouzel
                 {
                     RendererOGL::bindTexture(textureId, 0);
 
-                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
