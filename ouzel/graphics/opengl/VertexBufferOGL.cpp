523e4a0c396096025635132dc062a52cbae9ad8a
elnormous <elviss@elviss.lv>
2016-09-08 20:31:55 +0300

    Add missing cstring include

--- ouzel/graphics/opengl/VertexBufferOGL.cpp
+++ ouzel/graphics/opengl/VertexBufferOGL.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2016 Elviss Strazdins
 // This file is part of the Ouzel engine.
 
-#include <memory.h>
+#include <cstring>
 #include "VertexBufferOGL.h"
 #include "RendererOGL.h"
 
