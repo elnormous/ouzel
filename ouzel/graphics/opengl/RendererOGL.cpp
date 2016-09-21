7a9fd041ebeb5f81c96f45dc387bb575fdefb3e0
elnormous <elviss@elviss.lv>
2016-09-21 19:48:38 +0300

    Revert glGetString for Emscripten Delete files generated by Emscripten

--- ouzel/graphics/opengl/RendererOGL.cpp
+++ ouzel/graphics/opengl/RendererOGL.cpp
@@ -116,9 +116,6 @@ namespace ouzel
                 return false;
             }
 
-#if OUZEL_PLATFORM_EMSCRIPTEN
-            log(LOG_LEVEL_INFO, "Using WebGL for rendering");
-#else
             //const GLubyte* deviceVendor = glGetString(GL_VENDOR);
             const GLubyte* deviceName = glGetString(GL_RENDERER);
 
@@ -130,7 +127,6 @@ namespace ouzel
             {
                 log(LOG_LEVEL_INFO, "Using %s for rendering", reinterpret_cast<const char*>(deviceName));
             }
-#endif
 
 #if OUZEL_SUPPORTS_OPENGLES
             npotTexturesSupported = (apiMajorVersion >= 3);
