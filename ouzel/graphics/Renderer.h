bf9116d1d115649283ef276ae741562ddb5cdc0e
elnormous <elviss@elviss.lv>
2016-09-22 00:00:04 +0300

    Add more pixel formats

--- ouzel/graphics/Renderer.h
+++ ouzel/graphics/Renderer.h
@@ -73,7 +73,38 @@ namespace ouzel
             enum class PixelFormat
             {
                 DEFAULT,
-                R8G8B8A8_UNORM
+                R8_UNORM,
+                R8_SNORM,
+                R8_UINT,
+                R8_SINT,
+                R16_UNORM,
+                R16_SNORM,
+                R16_UINT,
+                R16_SINT,
+                R16_FLOAT,
+                R32_UINT,
+                R32_SINT,
+                R32_FLOAT,
+                RG8_UNORM,
+                RG8_SNORM,
+                RG8_UINT,
+                RG8_SINT,
+                RGB8_UNORM,
+                RGB8_SNORM,
+                RGB8_UINT,
+                RGB8_SINT,
+                RGBA8_UNORM,
+                RGBA8_SNORM,
+                RGBA8_UINT,
+                RGBA8_SINT,
+                RGBA16_UNORM,
+                RGBA16_SNORM,
+                RGBA16_UINT,
+                RGBA16_SINT,
+                RGBA16_FLOAT,
+                RGBA32_UINT,
+                RGBA32_SINT,
+                RGBA32_FLOAT
             };
 
             virtual ~Renderer();
