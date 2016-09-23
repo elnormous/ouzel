302e96d7c4264f0840b31af944b36b8996b524d2
elnormous <elviss@elviss.lv>
2016-09-23 05:28:16 +0300

    Fix iOS and tvOS OpenGL renderer constructors Move OpenGL shaders to shaders directory and add generate script

---
+++ shaders/opengl/generate.sh
@@ -0,0 +1,23 @@
+# OpenGL 2
+xxd -i ColorGL2.ps ../../ouzel/graphics/opengl/ColorPSGL2.h
+xxd -i ColorGL2.vs ../../ouzel/graphics/opengl/ColorVSGL2.h
+xxd -i TextureGL2.ps ../../ouzel/graphics/opengl/TexturePSGL2.h
+xxd -i TextureGL2.vs ../../ouzel/graphics/opengl/TextureVSGL2.h
+
+# OpenGL 3
+xxd -i ColorGL3.ps ../../ouzel/graphics/opengl/ColorPSGL3.h
+xxd -i ColorGL3.vs ../../ouzel/graphics/opengl/ColorVSGL3.h
+xxd -i TextureGL3.ps ../../ouzel/graphics/opengl/TexturePSGL3.h
+xxd -i TextureGL3.vs ../../ouzel/graphics/opengl/TextureVSGL3.h
+
+# OpenGL ES 2
+xxd -i ColorGLES2.ps ../../ouzel/graphics/opengl/ColorPSGLES2.h
+xxd -i ColorGLES2.vs ../../ouzel/graphics/opengl/ColorVSGLES2.h
+xxd -i TextureGLES2.ps ../../ouzel/graphics/opengl/TexturePSGLES2.h
+xxd -i TextureGLES2.vs ../../ouzel/graphics/opengl/TextureVSGLES2.h
+
+# OpenGL ES 3
+xxd -i ColorGLES3.ps ../../ouzel/graphics/opengl/ColorPSGLES3.h
+xxd -i ColorGLES3.vs ../../ouzel/graphics/opengl/ColorVSGLES3.h
+xxd -i TextureGLES3.ps ../../ouzel/graphics/opengl/TexturePSGLES3.h
+xxd -i TextureGLES3.vs ../../ouzel/graphics/opengl/TextureVSGLES3.h
