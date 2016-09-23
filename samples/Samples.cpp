ae47e445a9f1d54cb654ab85f57c8905971b4669
elnormous <elviss@elviss.lv>
2016-09-23 05:29:42 +0300

    Embed resource directory for Emscripten target

--- samples/Samples.cpp
+++ samples/Samples.cpp
@@ -17,7 +17,7 @@ void Samples::begin(const std::string& sample)
 {
     sharedEngine->getInput()->startGamepadDiscovery();
 
-#if OUZEL_PLATFORM_LINUX || OUZEL_PLATFORM_RASPBIAN
+#if OUZEL_PLATFORM_LINUX || OUZEL_PLATFORM_RASPBIAN || OUZEL_PLATFORM_EMSCRIPTEN
     sharedApplication->getFileSystem()->addResourcePath("Resources");
 #elif OUZEL_PLATFORM_WINDOWS
     sharedApplication->getFileSystem()->addResourcePath("Resources");
