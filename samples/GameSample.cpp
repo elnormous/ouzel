7adcc3e582fcc333789b7827fc884c10a7d73293
Elviss Strazdins <elviss@elviss.lv>
2016-11-19 14:14:55 +0200

    Add input handlers to game sample

--- samples/GameSample.cpp
+++ samples/GameSample.cpp
@@ -3,9 +3,45 @@
 
 #include "GameSample.h"
 
+using namespace std;
+using namespace ouzel;
+
 GameSample::GameSample(Samples& aSamples):
     samples(aSamples)
 {
+    eventHandler.keyboardHandler = bind(&GameSample::handleKeyboard, this, placeholders::_1, placeholders::_2);
+    eventHandler.mouseHandler = bind(&GameSample::handleMouse, this, placeholders::_1, placeholders::_2);
+    eventHandler.touchHandler = bind(&GameSample::handleTouch, this, placeholders::_1, placeholders::_2);
+    eventHandler.gamepadHandler = bind(&GameSample::handleGamepad, this, placeholders::_1, placeholders::_2);
+    eventHandler.uiHandler = bind(&GameSample::handleUI, this, placeholders::_1, placeholders::_2);
+
+    sharedEngine->getEventDispatcher()->addEventHandler(&eventHandler);
+
     addLayer(&layer);
     layer.addCamera(&camera);
+}
+
+bool GameSample::handleKeyboard(Event::Type type, const KeyboardEvent& event)
+{
+    return true;
+}
+
+bool GameSample::handleMouse(Event::Type type, const MouseEvent& event)
+{
+    return true;
+}
+
+bool GameSample::handleTouch(Event::Type, const TouchEvent& event)
+{
+    return true;
+}
+
+bool GameSample::handleGamepad(Event::Type type, const GamepadEvent& event)
+{
+    return true;
+}
+
+bool GameSample::handleUI(Event::Type type, const UIEvent& event) const
+{
+    return true;
 }
--- samples/GameSample.h
+++ samples/GameSample.h
@@ -11,8 +11,16 @@ public:
     GameSample(Samples& aSamples);
 
 private:
+    bool handleKeyboard(ouzel::Event::Type type, const ouzel::KeyboardEvent& event);
+    bool handleMouse(ouzel::Event::Type type, const ouzel::MouseEvent& event);
+    bool handleTouch(ouzel::Event::Type type, const ouzel::TouchEvent& event);
+    bool handleGamepad(ouzel::Event::Type type, const ouzel::GamepadEvent& event);
+    bool handleUI(ouzel::Event::Type type, const ouzel::UIEvent& event) const;
+    
     Samples& samples;
 
     ouzel::scene::Layer layer;
     ouzel::scene::Camera camera;
+
+    ouzel::EventHandler eventHandler;
 };
