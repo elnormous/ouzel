cfc68f837d39d5b63fab123543b68b0a4549d78d
Elviss Strazdins <elviss@elviss.lv>
2016-12-27 01:01:14 +0200

    Add OBF (Ouzel binary format) encoding and decoding

---
+++ ouzel/utils/OBF.cpp
@@ -0,0 +1,541 @@
+// Copyright (C) 2016 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#include "OBF.h"
+#include "Utils.h"
+
+namespace ouzel
+{
+    namespace obf
+    {
+        // reading
+        static uint32_t readInt8(const std::vector<uint8_t>& buffer, uint32_t offset, int8_t& result)
+        {
+            if (buffer.size() - offset < sizeof(result))
+            {
+                return 0;
+            }
+
+            result = *reinterpret_cast<const int8_t*>(buffer.data() + offset);
+
+            return sizeof(result);
+        }
+
+        static uint32_t readInt16(const std::vector<uint8_t>& buffer, uint32_t offset, int16_t& result)
+        {
+            if (buffer.size() - offset < sizeof(result))
+            {
+                return 0;
+            }
+
+            result = decodeInt16Big(buffer.data() + offset);
+
+            return sizeof(result);
+        }
+
+        static uint32_t readInt32(const std::vector<uint8_t>& buffer, uint32_t offset, int32_t& result)
+        {
+            if (buffer.size() - offset < sizeof(result))
+            {
+                return 0;
+            }
+
+            result = decodeInt32Big(buffer.data() + offset);
+
+            return sizeof(result);
+        }
+
+        static uint32_t readInt64(const std::vector<uint8_t>& buffer, uint32_t offset, int64_t& result)
+        {
+            if (buffer.size() - offset < sizeof(result))
+            {
+                return 0;
+            }
+
+            result = decodeInt64Big(buffer.data() + offset);
+
+            return sizeof(result);
+        }
+
+        static uint32_t readFloat(const std::vector<uint8_t>& buffer, uint32_t offset, float& result)
+        {
+            if (buffer.size() - offset < sizeof(float))
+            {
+                return 0;
+            }
+
+            result = *reinterpret_cast<const float*>(buffer.data() + offset);
+
+            return sizeof(result);
+        }
+
+        static uint32_t readDouble(const std::vector<uint8_t>& buffer, uint32_t offset, double& result)
+        {
+            if (buffer.size() - offset < sizeof(double))
+            {
+                return 0;
+            }
+
+            result = *reinterpret_cast<const double*>(buffer.data() + offset);
+
+            return sizeof(result);
+        }
+
+        static uint32_t readString(const std::vector<uint8_t>& buffer, uint32_t offset, std::string& result)
+        {
+            uint32_t originalOffset = offset;
+
+            if (buffer.size() - offset < sizeof(uint16_t))
+            {
+                return 0;
+            }
+
+            uint16_t length = decodeUInt16Big(buffer.data() + offset);
+
+            offset += sizeof(length);
+
+            if (buffer.size() - offset < length)
+            {
+                return 0;
+            }
+
+            result.assign(reinterpret_cast<const char*>(buffer.data() + offset), length);
+            offset += length;
+
+            return offset - originalOffset;
+        }
+
+        static uint32_t readLongString(const std::vector<uint8_t>& buffer, uint32_t offset, std::string& result)
+        {
+            uint32_t originalOffset = offset;
+
+            if (buffer.size() - offset < sizeof(uint32_t))
+            {
+                return 0;
+            }
+
+            uint32_t length = decodeUInt32Big(buffer.data() + offset);
+
+            offset += sizeof(length);
+
+            if (buffer.size() - offset < length)
+            {
+                return 0;
+            }
+
+            result.assign(reinterpret_cast<const char*>(buffer.data() + offset), length);
+            offset += length;
+            
+            return offset - originalOffset;
+        }
+
+        static uint32_t readByteArray(const std::vector<uint8_t>& buffer, uint32_t offset, std::vector<uint8_t>& result)
+        {
+            uint32_t originalOffset = offset;
+
+            if (buffer.size() - offset < sizeof(uint32_t))
+            {
+                return 0;
+            }
+
+            uint32_t length = decodeUInt32Big(buffer.data() + offset);
+
+            offset += sizeof(length);
+
+            if (buffer.size() - offset < length)
+            {
+                return 0;
+            }
+
+            result.assign(reinterpret_cast<const uint8_t*>(buffer.data() + offset),
+                          reinterpret_cast<const uint8_t*>(buffer.data() + offset) + length);
+            offset += length;
+            
+            return offset - originalOffset;
+        }
+
+        static uint32_t readObject(const std::vector<uint8_t>& buffer, uint32_t offset, std::map<uint32_t, Value>& result)
+        {
+            uint32_t originalOffset = offset;
+
+            if (buffer.size() - offset < sizeof(uint32_t))
+            {
+                return 0;
+            }
+
+            uint32_t count = decodeUInt32Big(buffer.data() + offset);
+
+            offset += sizeof(count);
+
+            for (uint32_t i = 0; i < count; ++i)
+            {
+                if (buffer.size() - offset < sizeof(uint32_t))
+                {
+                    return 0;
+                }
+
+                uint32_t key = decodeUInt32Big(buffer.data() + offset);
+
+                offset += sizeof(key);
+
+                Value node;
+
+                uint32_t ret = node.decode(buffer, offset);
+
+                if (ret == 0)
+                {
+                    return 0;
+                }
+                offset += ret;
+
+                result[static_cast<uint32_t>(key)] = node;
+            }
+
+            return offset - originalOffset;
+        }
+
+        static uint32_t readArray(const std::vector<uint8_t>& buffer, uint32_t offset, std::vector<Value>& result)
+        {
+            uint32_t originalOffset = offset;
+
+            if (buffer.size() - offset < sizeof(uint32_t))
+            {
+                return 0;
+            }
+
+            uint32_t count = decodeUInt32Big(buffer.data() + offset);
+
+            offset += sizeof(count);
+
+            for (uint32_t i = 0; i < count; ++i)
+            {
+                Value node;
+
+                if (!node.decode(buffer, offset))
+                {
+                    return 0;
+                }
+
+                result.push_back(node);
+            }
+
+            return offset - originalOffset;
+        }
+
+        // writing
+        static uint32_t writeInt8(std::vector<uint8_t>& buffer, int8_t value)
+        {
+            buffer.push_back(static_cast<uint8_t>(value));
+
+            return sizeof(value);
+        }
+
+        static uint32_t writeInt16(std::vector<uint8_t>& buffer, int16_t value)
+        {
+            uint8_t data[sizeof(value)];
+
+            encodeInt16Big(data, value);
+
+            buffer.insert(buffer.end(), std::begin(data), std::end(data));
+
+            return sizeof(value);
+        }
+
+        static uint32_t writeInt32(std::vector<uint8_t>& buffer, int32_t value)
+        {
+            uint8_t data[sizeof(value)];
+
+            encodeInt32Big(data, value);
+
+            buffer.insert(buffer.end(), std::begin(data), std::end(data));
+
+            return sizeof(value);
+        }
+
+        static uint32_t writeInt64(std::vector<uint8_t>& buffer, int64_t value)
+        {
+            uint8_t data[sizeof(value)];
+
+            encodeInt64Big(data, value);
+
+            buffer.insert(buffer.end(), std::begin(data), std::end(data));
+
+            return sizeof(value);
+        }
+
+        static uint32_t writeFloat(std::vector<uint8_t>& buffer, float value)
+        {
+            buffer.insert(buffer.begin(),
+                          reinterpret_cast<const uint8_t*>(&value),
+                          reinterpret_cast<const uint8_t*>(&value) + sizeof(value));
+
+            return sizeof(float);
+        }
+
+        static uint32_t writeDouble(std::vector<uint8_t>& buffer, double value)
+        {
+            buffer.insert(buffer.begin(),
+                          reinterpret_cast<const uint8_t*>(&value),
+                          reinterpret_cast<const uint8_t*>(&value) + sizeof(value));
+
+            return sizeof(double);
+        }
+
+        static uint32_t writeString(std::vector<uint8_t>& buffer, const std::string& value)
+        {
+            uint8_t lengthData[sizeof(uint16_t)];
+
+            encodeUInt16Big(lengthData, static_cast<uint16_t>(value.length()));
+
+            buffer.insert(buffer.end(), std::begin(lengthData), std::end(lengthData));
+
+            uint32_t size = sizeof(lengthData);
+
+            buffer.insert(buffer.end(),
+                          reinterpret_cast<const uint8_t*>(value.data()),
+                          reinterpret_cast<const uint8_t*>(value.data()) + value.length());
+            size += static_cast<uint32_t>(value.length());
+
+            return size;
+        }
+
+        static uint32_t writeLongString(std::vector<uint8_t>& buffer, const std::string& value)
+        {
+            uint8_t lengthData[sizeof(uint32_t)];
+
+            encodeUInt32Big(lengthData, static_cast<uint32_t>(value.length()));
+
+            buffer.insert(buffer.end(), std::begin(lengthData), std::end(lengthData));
+
+            uint32_t size = sizeof(lengthData);
+
+            buffer.insert(buffer.end(),
+                          reinterpret_cast<const uint8_t*>(value.data()),
+                          reinterpret_cast<const uint8_t*>(value.data()) + value.length());
+            size += static_cast<uint32_t>(value.length());
+            
+            return size;
+        }
+
+        static uint32_t writeByteArray(std::vector<uint8_t>& buffer, const std::vector<uint8_t>& value)
+        {
+            uint8_t lengthData[sizeof(uint32_t)];
+
+            encodeUInt32Big(lengthData, static_cast<uint32_t>(value.size()));
+
+            buffer.insert(buffer.end(), std::begin(lengthData), std::end(lengthData));
+
+            uint32_t size = sizeof(lengthData);
+
+            buffer.insert(buffer.end(), value.begin(), value.end());
+            size += static_cast<uint32_t>(value.size());
+
+            return size;
+        }
+
+        static uint32_t writeObject(std::vector<uint8_t>& buffer, const std::map<uint32_t, Value>& value)
+        {
+            uint8_t lengthData[sizeof(uint32_t)];
+
+            encodeUInt32Big(lengthData, static_cast<uint32_t>(value.size()));
+
+            buffer.insert(buffer.end(), std::begin(lengthData), std::end(lengthData));
+
+            uint32_t size = sizeof(lengthData);
+
+            for (const auto& i : value)
+            {
+                uint8_t keyData[sizeof(uint32_t)];
+
+                encodeUInt32Big(keyData, i.first);
+
+                buffer.insert(buffer.end(), std::begin(keyData), std::end(keyData));
+
+                size += sizeof(keyData);
+
+                size += i.second.encode(buffer);
+            }
+
+            return size;
+        }
+
+        static uint32_t writeArray(std::vector<uint8_t>& buffer, const std::vector<Value>& value)
+        {
+            uint8_t lengthData[sizeof(uint32_t)];
+
+            encodeUInt32Big(lengthData, static_cast<uint32_t>(value.size()));
+
+            buffer.insert(buffer.end(), std::begin(lengthData), std::end(lengthData));
+
+            uint32_t size = sizeof(lengthData);
+
+            for (const auto& i : value)
+            {
+                size += i.encode(buffer);
+            }
+
+            return size;
+        }
+
+        uint32_t Value::decode(const std::vector<uint8_t>& buffer, uint32_t offset)
+        {
+            uint32_t originalOffset = offset;
+
+            if (buffer.size() - offset < 1)
+            {
+                return 0;
+            }
+
+            type = *reinterpret_cast<const Type*>(buffer.data() + offset);
+            offset += 1;
+
+            uint32_t ret = 0;
+
+            switch (type)
+            {
+                case Type::NONE:
+                {
+                    break;
+                }
+                case Type::INT8:
+                {
+                    int8_t int8Value;
+                    if ((ret = readInt8(buffer, offset, int8Value)) == 0)
+                    {
+                        return 0;
+                    }
+
+                    intValue = int8Value;
+                    break;
+                }
+                case Type::INT16:
+                {
+                    int16_t int16Value;
+                    if ((ret = readInt16(buffer, offset, int16Value)) == 0)
+                    {
+                        return 0;
+                    }
+
+                    intValue = int16Value;
+                    break;
+                }
+                case Type::INT32:
+                {
+                    int32_t int32Value;
+                    if ((ret = readInt32(buffer, offset, int32Value)) == 0)
+                    {
+                        return 0;
+                    }
+
+                    intValue = int32Value;
+                    break;
+                }
+                case Type::INT64:
+                {
+                    if ((ret = readInt64(buffer, offset, intValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                case Type::FLOAT:
+                {
+                    float floatValue;
+                    if ((ret = readFloat(buffer, offset, floatValue)) == 0)
+                    {
+                        return 0;
+                    }
+
+                    doubleValue = floatValue;
+                    break;
+                }
+                case Type::DOUBLE:
+                {
+                    if ((ret = readDouble(buffer, offset, doubleValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                case Type::STRING:
+                {
+                    if ((ret = readString(buffer, offset, stringValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                case Type::LONG_STRING:
+                {
+                    if ((ret = readLongString(buffer, offset, stringValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                case Type::BYTE_ARRAY:
+                {
+                    if ((ret = readByteArray(buffer, offset, byteArrayValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                case Type::OBJECT:
+                {
+                    if ((ret = readObject(buffer, offset, mapValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                case Type::ARRAY:
+                {
+                    if ((ret = readArray(buffer, offset, vectorValue)) == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+                default: return 0;
+            }
+            
+            offset += ret;
+            
+            return offset - originalOffset;
+        }
+        
+        uint32_t Value::encode(std::vector<uint8_t>& buffer) const
+        {
+            uint32_t size = 0;
+            
+            uint32_t ret = 0;
+            
+            buffer.push_back(static_cast<uint8_t>(type));
+            size += 1;
+            
+            switch (type)
+            {
+                case Type::NONE: break;
+
+                case Type::INT8: ret = writeInt8(buffer, static_cast<int8_t>(intValue)); break;
+                case Type::INT16: ret = writeInt16(buffer, static_cast<int16_t>(intValue)); break;
+                case Type::INT32: ret = writeInt32(buffer, static_cast<int32_t>(intValue)); break;
+                case Type::INT64: ret = writeInt64(buffer, intValue); break;
+                case Type::FLOAT: ret = writeFloat(buffer, static_cast<float>(doubleValue)); break;
+                case Type::DOUBLE: ret = writeDouble(buffer, doubleValue); break;
+                case Type::STRING: ret = writeString(buffer, stringValue); break;
+                case Type::LONG_STRING: ret = writeLongString(buffer, stringValue); break;
+                case Type::BYTE_ARRAY: ret = writeByteArray(buffer, byteArrayValue); break;
+                case Type::OBJECT: ret = writeObject(buffer, mapValue); break;
+                case Type::ARRAY: ret = writeArray(buffer, vectorValue); break;
+
+                default: return 0;
+            }
+            
+            size += ret;
+            
+            return size;
+        }
+    } // namespace obf
+} // namespace ouzel
---
+++ ouzel/utils/OBF.h
@@ -0,0 +1,237 @@
+// Copyright (C) 2016 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <map>
+#include <string>
+#include <vector>
+
+namespace ouzel
+{
+    namespace obf
+    {
+        class Value
+        {
+        public:
+            enum class Type: uint8_t
+            {
+                NONE,
+                INT8,
+                INT16,
+                INT32,
+                INT64,
+                FLOAT,
+                DOUBLE,
+                STRING,
+                LONG_STRING,
+                BYTE_ARRAY,
+                OBJECT,
+                ARRAY
+            };
+
+            Value() {}
+            Value(Type aType): type(aType) {}
+            Value(int8_t value): type(Type::INT8), intValue(value) {}
+            Value(int16_t value): type(Type::INT16), intValue(value) {}
+            Value(int32_t value): type(Type::INT32), intValue(value) {}
+            Value(int64_t value): type(Type::INT64), intValue(value) {}
+            Value(float value): type(Type::FLOAT), doubleValue(value) {}
+            Value(double value): type(Type::DOUBLE), doubleValue(value) {}
+            Value(const std::string& value);
+
+            Value& operator=(Type newType)
+            {
+                type = newType;
+
+                return *this;
+            }
+
+            Value& operator=(float value)
+            {
+                type = Type::FLOAT;
+                doubleValue = value;
+
+                return *this;
+            }
+
+            Value& operator=(double value)
+            {
+                type = Type::DOUBLE;
+                doubleValue = value;
+
+                return *this;
+            }
+
+            Value& operator=(const std::string& value)
+            {
+                stringValue = value;
+                if (value.length() <= UINT16_MAX)
+                {
+                    type = Type::STRING;
+                }
+                else
+                {
+                    type = Type::LONG_STRING;
+                }
+                return *this;
+            }
+
+            Type getType() const { return type; }
+
+            uint32_t decode(const std::vector<uint8_t>& buffer, uint32_t offset = 0);
+            uint32_t encode(std::vector<uint8_t>& buffer) const;
+
+            int8_t asInt8() const
+            {
+                assert(type == Type::INT8 || type == Type::INT16 || type == Type::INT32 || type == Type::INT64);
+
+                return static_cast<int8_t>(intValue);
+            }
+
+            int16_t asInt16() const
+            {
+                assert(type == Type::INT8 || type == Type::INT16 || type == Type::INT32 || type == Type::INT64);
+
+                return static_cast<int16_t>(intValue);
+            }
+
+            int32_t asInt32() const
+            {
+                assert(type == Type::INT8 || type == Type::INT16 || type == Type::INT32 || type == Type::INT64);
+
+                return static_cast<int32_t>(intValue);
+            }
+
+            int64_t asInt64() const
+            {
+                assert(type == Type::INT8 || type == Type::INT16 || type == Type::INT32 || type == Type::INT64);
+
+                return intValue;
+            }
+
+            float asFloat() const
+            {
+                assert(type == Type::FLOAT || type == Type::DOUBLE);
+
+                return static_cast<float>(doubleValue);
+            }
+
+            double asDouble() const
+            {
+                assert(type == Type::FLOAT || type == Type::DOUBLE);
+
+                return doubleValue;
+            }
+
+            const std::string& asString() const
+            {
+                assert(type == Type::STRING || type == Type::LONG_STRING);
+
+                return stringValue;
+            }
+
+            const std::vector<Value>& asVector() const
+            {
+                assert(type == Type::ARRAY);
+
+                return vectorValue;
+            }
+
+            const std::map<uint32_t, Value>& asMap() const
+            {
+                assert(type == Type::OBJECT);
+
+                return mapValue;
+            }
+
+            const std::vector<uint8_t>& asByteArray() const
+            {
+                assert(type == Type::BYTE_ARRAY);
+
+                return byteArrayValue;
+            }
+
+            uint32_t getSize() const
+            {
+                assert(type == Type::ARRAY);
+
+                return static_cast<uint32_t>(vectorValue.size());
+            }
+
+            Value operator[](uint32_t key) const
+            {
+                assert(type == Type::OBJECT || type == Type::ARRAY);
+
+                if (type == Type::OBJECT)
+                {
+                    auto i = mapValue.find(key);
+
+                    if (i != mapValue.end())
+                    {
+                        return i->second;
+                    }
+                }
+                else if (type == Type::ARRAY)
+                {
+                    if (key < vectorValue.size())
+                    {
+                        return vectorValue[key];
+                    }
+                }
+
+                return Value();
+            }
+
+            Value& operator[](uint32_t key)
+            {
+                assert(type == Type::OBJECT || type == Type::ARRAY);
+
+                if (type == Type::OBJECT)
+                {
+                    return mapValue[key];
+                }
+                else
+                {
+                    type = Type::ARRAY;
+
+                    return vectorValue[key];
+                }
+            }
+
+            bool hasElement(uint32_t key) const
+            {
+                assert(type == Type::OBJECT || type == Type::ARRAY);
+
+                if (type == Type::OBJECT)
+                {
+                    return mapValue.find(key) != mapValue.end();
+                }
+                else if (type == Type::ARRAY)
+                {
+                    return key < vectorValue.size();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            void append(const Value& node)
+            {
+                vectorValue.push_back(node);
+            }
+
+        private:
+            Type type = Type::NONE;
+            int64_t intValue;
+            double doubleValue;
+            std::string stringValue;
+            std::vector<Value> vectorValue;
+            std::map<uint32_t, Value> mapValue;
+            std::vector<uint8_t> byteArrayValue;
+        };
+    } // namespace obf
+} // namespace ouzel
