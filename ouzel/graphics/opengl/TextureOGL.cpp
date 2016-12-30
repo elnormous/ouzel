08be680644ec830d6f42b32cae4e62ee1208a446
Elviss Strazdins <elviss@elviss.lv>
2016-12-30 12:06:28 +0200

    Fix OpenGL texture mag filter for linear filtering

--- ouzel/graphics/opengl/TextureOGL.cpp
+++ ouzel/graphics/opengl/TextureOGL.cpp
@@ -71,7 +71,7 @@ namespace ouzel
                                 break;
                             case TextureFilter::LINEAR:
                                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-                                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                                 break;
                             case TextureFilter::BILINEAR:
                                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
