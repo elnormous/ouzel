8388a8edaac23ca36cdb184e7721e3767ed1c6b0
Elviss Strazdins <elviss@elviss.lv>
2017-01-11 02:40:50 +0200

    Implement multisampling for Direct3D 11 texture

--- ouzel/graphics/direct3d11/TextureD3D11.cpp
+++ ouzel/graphics/direct3d11/TextureD3D11.cpp
@@ -87,7 +87,7 @@ namespace ouzel
                     textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
                     textureDesc.Usage = (uploadData.dynamic && !uploadData.renderTarget) ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
                     textureDesc.CPUAccessFlags = (uploadData.dynamic && !uploadData.renderTarget) ? D3D11_CPU_ACCESS_WRITE : 0;
-                    textureDesc.SampleDesc.Count = 1;
+                    textureDesc.SampleDesc.Count = uploadData.sampleCount;
                     textureDesc.SampleDesc.Quality = 0;
                     textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | (uploadData.renderTarget ? D3D11_BIND_RENDER_TARGET : 0);
                     textureDesc.MiscFlags = 0;
@@ -99,13 +99,17 @@ namespace ouzel
                         return false;
                     }
 
-                    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
-                    srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-                    srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-                    srvDesc.Texture2D.MostDetailedMip = 0;
-                    srvDesc.Texture2D.MipLevels = uploadData.mipmaps ? static_cast<UINT>(uploadData.levels.size()) : 1;
+                    D3D11_SHADER_RESOURCE_VIEW_DESC resourceViewDesc;
+                    resourceViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+                    resourceViewDesc.ViewDimension = (uploadData.sampleCount > 1) ? D3D11_SRV_DIMENSION_TEXTURE2DMS : D3D11_SRV_DIMENSION_TEXTURE2D;
+                    
+                    if (uploadData.sampleCount == 1)
+                    {
+                        resourceViewDesc.Texture2D.MostDetailedMip = 0;
+                        resourceViewDesc.Texture2D.MipLevels = uploadData.mipmaps ? static_cast<UINT>(uploadData.levels.size()) : 1;
+                    }
 
-                    hr = rendererD3D11->getDevice()->CreateShaderResourceView(texture, &srvDesc, &resourceView);
+                    hr = rendererD3D11->getDevice()->CreateShaderResourceView(texture, &resourceViewDesc, &resourceView);
                     if (FAILED(hr))
                     {
                         Log(Log::Level::ERR) << "Failed to create Direct3D 11 shader resource view";
@@ -118,8 +122,12 @@ namespace ouzel
                         {
                             D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
                             renderTargetViewDesc.Format = textureDesc.Format;
-                            renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
-                            renderTargetViewDesc.Texture2D.MipSlice = 0;
+                            renderTargetViewDesc.ViewDimension = (uploadData.sampleCount > 1) ? D3D11_RTV_DIMENSION_TEXTURE2DMS : D3D11_RTV_DIMENSION_TEXTURE2D;
+
+                            if (uploadData.sampleCount == 1)
+                            {
+                                renderTargetViewDesc.Texture2D.MipSlice = 0;
+                            }
 
                             HRESULT hr = rendererD3D11->getDevice()->CreateRenderTargetView(texture, &renderTargetViewDesc, &renderTargetView);
                             if (FAILED(hr))
