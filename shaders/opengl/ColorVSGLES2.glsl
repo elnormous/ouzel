66b7337b1efe68385351a22488e462a019423dc1
Elviss Strazdins <elviss@elviss.lv>
2017-02-01 00:37:08 +0200

    Remove one remaining underscore

--- shaders/opengl/ColorVSGLES2.glsl
+++ shaders/opengl/ColorVSGLES2.glsl
@@ -2,7 +2,7 @@ precision highp float;
 attribute vec3 inPosition;
 attribute vec4 inColor;
 uniform mat4 modelViewProj;
-varying lowp vec4 ex_Color;
+varying lowp vec4 exColor;
 void main()
 {
     gl_Position = modelViewProj * vec4(inPosition, 1.0);
