8ccbb6426dc5c780d321b8a3a76c7ebd03e0cde4
Elviss Strazdins <elviss@elviss.lv>
2017-03-08 18:06:36 +0200

    Get rid of shared_from_this

--- ouzel/gui/CheckBox.cpp
+++ ouzel/gui/CheckBox.cpp
@@ -123,7 +123,7 @@ namespace ouzel
 
                     Event changeEvent;
                     changeEvent.type = Event::Type::UI_WIDGET_CHANGE;
-                    changeEvent.uiEvent.node = std::static_pointer_cast<Node>(shared_from_this());
+                    changeEvent.uiEvent.node = event.node;
                     sharedEngine->getEventDispatcher()->postEvent(changeEvent);
                 }
             }
