797f9b130a1808c2ad1b09fb6e6672ca64a0c52c
Elviss Strazdins <elviss@elviss.lv>
2017-04-07 00:12:13 +0300

    Fix Emscripten build

--- ouzel/core/emscripten/ApplicationEm.cpp
+++ ouzel/core/emscripten/ApplicationEm.cpp
@@ -47,7 +47,7 @@ namespace ouzel
     {
         executeAll();
         sharedEngine->update();
-        sharedEngine->getAudio->update();
+        sharedEngine->getAudio()->update();
 
         if (!sharedEngine->draw())
         {
