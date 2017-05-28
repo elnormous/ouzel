6ba523f48e9d257313ae2cac92b4209d34440176
Elviss Strazdins <elviss@elviss.lv>
2017-05-29 00:31:20 +0300

    Add classes for Windows, macOS, and Linux cursor resources

---
+++ ouzel/core/linux/CursorResourceLinux.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2017 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#pragma once
+
+namespace ouzel
+{
+    class CursorResourceLinux
+    {
+    public:
+    };
+}
