f591761ccb9e80aace72d5c4c6187cc4cf7d1d48
Elviss Strazdins <elviss@elviss.lv>
2017-07-05 00:52:52 +0300

    Change order of includes

--- ouzel/audio/SoundDataVorbis.cpp
+++ ouzel/audio/SoundDataVorbis.cpp
@@ -1,13 +1,13 @@
 // Copyright (C) 2017 Elviss Strazdins
 // This file is part of the Ouzel engine.
 
-#include "stb_vorbis.c"
 #include "SoundDataVorbis.h"
 #include "StreamVorbis.h"
 #include "core/Engine.h"
 #include "files/FileSystem.h"
 #include "utils/Log.h"
 #include "utils/Utils.h"
+#include "stb_vorbis.c"
 
 namespace ouzel
 {
