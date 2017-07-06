5db6588dc275aa5bc0de3e36905a1c63a5ace727
Elviss Strazdins <elviss@elviss.lv>
2017-07-06 13:46:25 +0300

    Add Microsoft DirectX SDK (June 2010) includes for XAudio 2.7 support

--- ouzel/audio/xaudio2/XAudio27.h
+++ ouzel/audio/xaudio2/XAudio27.h
@@ -9,7 +9,7 @@
 
 #include <xaudio2.h>
 
-HRESULT XAudio27CreateProc(IXAudio2** ppXAudio2, UINT32 Flags, XAUDIO2_PROCESSOR XAudio2Processor);
+HRESULT XAudio27CreateProc(IXAudio2** ppXAudio2, UINT32 Flags, UINT32 XAudio2Processor);
 ULONG IXAudio2Release(IXAudio2* pXAudio2);
 HRESULT IXAudio2CreateMasteringVoice(IXAudio2* pXAudio2, IXAudio2MasteringVoice** ppMasteringVoice);
 HRESULT IXAudio2CreateSourceVoice(IXAudio2* pXAudio2,
