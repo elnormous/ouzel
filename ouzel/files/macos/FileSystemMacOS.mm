68608758f44e6d3c508cf2cc4a1c52e5e3687141
Elviss Strazdins <elviss@elviss.lv>
2017-07-13 22:55:11 +0300

    Use bundle identifier

--- ouzel/files/macos/FileSystemMacOS.mm
+++ ouzel/files/macos/FileSystemMacOS.mm
@@ -5,9 +5,6 @@
 #include "FileSystemMacOS.h"
 #include "utils/Log.h"
 
-extern std::string DEVELOPER_NAME;
-extern std::string APPLICATION_NAME;
-
 namespace ouzel
 {
     FileSystemMacOS::FileSystemMacOS()
@@ -22,23 +19,22 @@ namespace ouzel
 
     std::string FileSystemMacOS::getStorageDirectory(bool user) const
     {
-        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, user ? NSUserDomainMask : NSLocalDomainMask, YES);
+        NSError* error;
+        NSURL* applicationSupportDirectory = [fileManager URLForDirectory:NSApplicationSupportDirectory inDomain:user ? NSUserDomainMask : NSLocalDomainMask appropriateForURL:nil create:YES error:&error];
 
-        if ([paths count] == 0)
+        if (!applicationSupportDirectory)
         {
             Log(Log::Level::ERR) << "Failed to get application support directory";
             return "";
         }
 
-        NSString* applicationSupportDirectory = [paths firstObject];
-        NSString* path = [NSString stringWithFormat:@"%@/%s/%s", applicationSupportDirectory, DEVELOPER_NAME.c_str(), APPLICATION_NAME.c_str()];
+        NSString* identifier = [[NSBundle mainBundle] bundleIdentifier];
 
-        if (![fileManager fileExistsAtPath:path isDirectory:Nil])
-        {
-            [fileManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:Nil error:Nil];
-        }
+        NSURL* path = [applicationSupportDirectory URLByAppendingPathComponent:identifier];
 
-        return [path UTF8String];
+        [fileManager createDirectoryAtURL:path withIntermediateDirectories:YES attributes:Nil error:Nil];
+
+        return [[path path] UTF8String];
     }
 
     std::string FileSystemMacOS::getTempDirectory() const
