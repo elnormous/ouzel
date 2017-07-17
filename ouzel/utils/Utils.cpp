792f706b3751c7d7f851f2803993b60da976b9f1
Elviss Strazdins <elviss@elviss.lv>
2017-07-17 17:32:23 +0300

    Use stdlib's conversion functions to avoid exceptions

--- ouzel/utils/Utils.cpp
+++ ouzel/utils/Utils.cpp
@@ -1,33 +1,11 @@
 // Copyright (C) 2017 Elviss Strazdins
 // This file is part of the Ouzel engine.
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "Utils.h"
 
 namespace ouzel
 {
 #if OUZEL_PLATFORM_ANDROID
-    long stringToLong(const std::string& val)
-    {
-        return strtol(val.c_str(), nullptr, 0);
-    }
-
-    long long stringToLongLong(const std::string& val)
-    {
-        return strtoll(val.c_str(), nullptr, 0);
-    }
-
-    float stringToFloat(const std::string& val)
-    {
-        return strtof(val.c_str(), nullptr);
-    }
-
-    double stringToDouble(const std::string& val)
-    {
-        return strtod(val.c_str(), nullptr);
-    }
-
     #if OUZEL_SUPPORTS_NEON_CHECK
         AnrdoidNEONChecker anrdoidNEONChecker;
     #endif
