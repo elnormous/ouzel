25d4a78381a18bfabe2ce8d2d8501ffd0a85a16e
Elviss Strazdins <elviss@elviss.lv>
2017-07-22 01:13:22 +0300

    Implement ShaderResource init for all render backends

--- ouzel/graphics/direct3d11/ShaderResourceD3D11.cpp
+++ ouzel/graphics/direct3d11/ShaderResourceD3D11.cpp
@@ -46,38 +46,6 @@ namespace ouzel
             }
         }
 
-        bool ShaderResourceD3D11::init(const std::vector<uint8_t>& newPixelShader,
-                                       const std::vector<uint8_t>& newVertexShader,
-                                       const std::vector<VertexAttribute>& newVertexAttributes,
-                                       const std::vector<Shader::ConstantInfo>& newPixelShaderConstantInfo,
-                                       const std::vector<Shader::ConstantInfo>& newVertexShaderConstantInfo,
-                                       uint32_t newPixelShaderDataAlignment,
-                                       uint32_t newVertexShaderDataAlignment,
-                                       const std::string& pixelShaderFunction,
-                                       const std::string& vertexShaderFunction)
-        {
-            return true;
-        }
-
-        bool ShaderResourceD3D11::uploadBuffer(ID3D11Buffer* buffer, const void* data, uint32_t size)
-        {
-            RendererD3D11* rendererD3D11 = static_cast<RendererD3D11*>(sharedEngine->getRenderer());
-
-            D3D11_MAPPED_SUBRESOURCE mappedSubresource;
-            HRESULT hr = rendererD3D11->getContext()->Map(buffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedSubresource);
-            if (FAILED(hr))
-            {
-                Log(Log::Level::ERR) << "Failed to lock Direct3D 11 buffer, error: " << hr;
-                return false;
-            }
-
-            std::copy(static_cast<const uint8_t*>(data), static_cast<const uint8_t*>(data) + size, static_cast<uint8_t*>(mappedSubresource.pData));
-
-            rendererD3D11->getContext()->Unmap(buffer, 0);
-
-            return true;
-        }
-
         static DXGI_FORMAT getVertexFormat(DataType dataType, bool normalized)
         {
             switch (dataType)
@@ -148,6 +116,228 @@ namespace ouzel
                 default:
                     return DXGI_FORMAT_UNKNOWN;
             }
+        }
+
+        bool ShaderResourceD3D11::init(const std::vector<uint8_t>& newPixelShader,
+                                       const std::vector<uint8_t>& newVertexShader,
+                                       const std::vector<VertexAttribute>& newVertexAttributes,
+                                       const std::vector<Shader::ConstantInfo>& newPixelShaderConstantInfo,
+                                       const std::vector<Shader::ConstantInfo>& newVertexShaderConstantInfo,
+                                       uint32_t newPixelShaderDataAlignment,
+                                       uint32_t newVertexShaderDataAlignment,
+                                       const std::string& pixelShaderFunction,
+                                       const std::string& vertexShaderFunction)
+        {
+            if (!ShaderResource::init(newPixelShader,
+                                      newVertexShader,
+                                      newVertexAttributes,
+                                      newPixelShaderConstantInfo,
+                                      newVertexShaderConstantInfo,
+                                      newPixelShaderDataAlignment,
+                                      newVertexShaderDataAlignment,
+                                      pixelShaderFunction,
+                                      vertexShaderFunction))
+            {
+                return false;
+            }
+
+            RendererD3D11* rendererD3D11 = static_cast<RendererD3D11*>(sharedEngine->getRenderer());
+
+            if (pixelShader)
+            {
+                pixelShader->Release();
+                pixelShader = nullptr;
+            }
+
+            if (vertexShader)
+            {
+                vertexShader->Release();
+                vertexShader = nullptr;
+            }
+
+            if (inputLayout)
+            {
+                inputLayout->Release();
+                inputLayout = nullptr;
+            }
+
+            if (pixelShaderConstantBuffer)
+            {
+                pixelShaderConstantBuffer->Release();
+                pixelShaderConstantBuffer = nullptr;
+            }
+
+            if (vertexShaderConstantBuffer)
+            {
+                vertexShaderConstantBuffer->Release();
+                vertexShaderConstantBuffer = nullptr;
+            }
+
+            HRESULT hr = rendererD3D11->getDevice()->CreatePixelShader(pixelShaderData.data(), pixelShaderData.size(), NULL, &pixelShader);
+            if (FAILED(hr))
+            {
+                Log(Log::Level::ERR) << "Failed to create a Direct3D 11 pixel shader, error: " << hr;
+                return false;
+            }
+            
+            hr = rendererD3D11->getDevice()->CreateVertexShader(vertexShaderData.data(), vertexShaderData.size(), NULL, &vertexShader);
+            if (FAILED(hr))
+            {
+                Log(Log::Level::ERR) << "Failed to create a Direct3D 11 vertex shader, error: " << hr;
+                return false;
+            }
+
+            std::vector<D3D11_INPUT_ELEMENT_DESC> vertexInputElements;
+
+            UINT offset = 0;
+
+            for (const VertexAttribute& vertexAttribute : vertexAttributes)
+            {
+                DXGI_FORMAT vertexFormat = getVertexFormat(vertexAttribute.dataType, vertexAttribute.normalized);
+
+                if (vertexFormat == DXGI_FORMAT_UNKNOWN)
+                {
+                    Log(Log::Level::ERR) << "Invalid vertex format";
+                    return false;
+                }
+
+                const char* usage;
+
+                switch (vertexAttribute.usage)
+                {
+                    case VertexAttribute::Usage::BINORMAL:
+                        usage = "BINORMAL";
+                        break;
+                    case VertexAttribute::Usage::BLEND_INDICES:
+                        usage = "BLENDINDICES";
+                        break;
+                    case VertexAttribute::Usage::BLEND_WEIGHT:
+                        usage = "BLENDWEIGHT";
+                        break;
+                    case VertexAttribute::Usage::COLOR:
+                        usage = "COLOR";
+                        break;
+                    case VertexAttribute::Usage::NORMAL:
+                        usage = "NORMAL";
+                        break;
+                    case VertexAttribute::Usage::POSITION:
+                        usage = "POSITION";
+                        break;
+                    case VertexAttribute::Usage::POSITION_TRANSFORMED:
+                        usage = "POSITIONT";
+                        break;
+                    case VertexAttribute::Usage::POINT_SIZE:
+                        usage = "PSIZE";
+                        break;
+                    case VertexAttribute::Usage::TANGENT:
+                        usage = "TANGENT";
+                        break;
+                    case VertexAttribute::Usage::TEXTURE_COORDINATES:
+                        usage = "TEXCOORD";
+                        break;
+                    default:
+                        Log(Log::Level::ERR) << "Invalid vertex attribute usage";
+                        return false;
+                }
+
+                vertexInputElements.push_back({
+                    usage, vertexAttribute.index,
+                    vertexFormat,
+                    0, offset, D3D11_INPUT_PER_VERTEX_DATA, 0
+                });
+                offset += getDataTypeSize(vertexAttribute.dataType);
+            }
+
+            hr = rendererD3D11->getDevice()->CreateInputLayout(
+                vertexInputElements.data(),
+                static_cast<UINT>(vertexInputElements.size()),
+                vertexShaderData.data(),
+                vertexShaderData.size(),
+                &inputLayout);
+            if (FAILED(hr))
+            {
+                Log(Log::Level::ERR) << "Failed to create Direct3D 11 input layout for vertex shader, error: " << hr;
+                return false;
+            }
+
+            if (!pixelShaderConstantInfo.empty())
+            {
+                pixelShaderConstantLocations.clear();
+                pixelShaderConstantLocations.reserve(pixelShaderConstantInfo.size());
+
+                pixelShaderConstantSize = 0;
+
+                for (const Shader::ConstantInfo& info : pixelShaderConstantInfo)
+                {
+                    pixelShaderConstantLocations.push_back({pixelShaderConstantSize, info.size});
+                    pixelShaderConstantSize += info.size;
+                }
+            }
+
+            D3D11_BUFFER_DESC pixelShaderConstantBufferDesc;
+            pixelShaderConstantBufferDesc.ByteWidth = static_cast<UINT>(pixelShaderConstantSize);
+            pixelShaderConstantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+            pixelShaderConstantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+            pixelShaderConstantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+            pixelShaderConstantBufferDesc.MiscFlags = 0;
+            pixelShaderConstantBufferDesc.StructureByteStride = 0;
+
+            hr = rendererD3D11->getDevice()->CreateBuffer(&pixelShaderConstantBufferDesc, nullptr, &pixelShaderConstantBuffer);
+            if (FAILED(hr))
+            {
+                Log(Log::Level::ERR) << "Failed to create Direct3D 11 constant buffer, error: " << hr;
+                return false;
+            }
+
+            if (!vertexShaderConstantInfo.empty())
+            {
+                vertexShaderConstantLocations.clear();
+                vertexShaderConstantLocations.reserve(vertexShaderConstantInfo.size());
+
+                vertexShaderConstantSize = 0;
+
+                for (const Shader::ConstantInfo& info : vertexShaderConstantInfo)
+                {
+                    vertexShaderConstantLocations.push_back({vertexShaderConstantSize, info.size});
+                    vertexShaderConstantSize += info.size;
+                }
+            }
+
+            D3D11_BUFFER_DESC vertexShaderConstantBufferDesc;
+            vertexShaderConstantBufferDesc.ByteWidth = static_cast<UINT>(vertexShaderConstantSize);
+            vertexShaderConstantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+            vertexShaderConstantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+            vertexShaderConstantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+            vertexShaderConstantBufferDesc.MiscFlags = 0;
+            vertexShaderConstantBufferDesc.StructureByteStride = 0;
+
+            hr = rendererD3D11->getDevice()->CreateBuffer(&vertexShaderConstantBufferDesc, nullptr, &vertexShaderConstantBuffer);
+            if (FAILED(hr))
+            {
+                Log(Log::Level::ERR) << "Failed to create Direct3D 11 constant buffer, error: " << hr;
+                return false;
+            }
+
+            return true;
+        }
+
+        bool ShaderResourceD3D11::uploadBuffer(ID3D11Buffer* buffer, const void* data, uint32_t size)
+        {
+            RendererD3D11* rendererD3D11 = static_cast<RendererD3D11*>(sharedEngine->getRenderer());
+
+            D3D11_MAPPED_SUBRESOURCE mappedSubresource;
+            HRESULT hr = rendererD3D11->getContext()->Map(buffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedSubresource);
+            if (FAILED(hr))
+            {
+                Log(Log::Level::ERR) << "Failed to lock Direct3D 11 buffer, error: " << hr;
+                return false;
+            }
+
+            std::copy(static_cast<const uint8_t*>(data), static_cast<const uint8_t*>(data) + size, static_cast<uint8_t*>(mappedSubresource.pData));
+
+            rendererD3D11->getContext()->Unmap(buffer, 0);
+
+            return true;
         }
 
         bool ShaderResourceD3D11::upload()
--- ouzel/graphics/metal/ShaderResourceMetal.mm
+++ ouzel/graphics/metal/ShaderResourceMetal.mm
@@ -110,6 +110,131 @@ namespace ouzel
                                        const std::string& pixelShaderFunction,
                                        const std::string& vertexShaderFunction)
         {
+            if (!ShaderResource::init(newPixelShader,
+                                      newVertexShader,
+                                      newVertexAttributes,
+                                      newPixelShaderConstantInfo,
+                                      newVertexShaderConstantInfo,
+                                      newPixelShaderDataAlignment,
+                                      newVertexShaderDataAlignment,
+                                      pixelShaderFunction,
+                                      vertexShaderFunction))
+            {
+                return false;
+            }
+
+            if (pixelShader)
+            {
+                [pixelShader release];
+                pixelShader = Nil;
+            }
+
+            if (vertexShader)
+            {
+                [vertexShader release];
+                vertexShader = Nil;
+            }
+
+            RendererMetal* rendererMetal = static_cast<RendererMetal*>(sharedEngine->getRenderer());
+
+            uint32_t index = 0;
+            NSUInteger offset = 0;
+
+            vertexDescriptor = [MTLVertexDescriptor new];
+
+            for (const VertexAttribute& vertexAttribute : vertexAttributes)
+            {
+                MTLVertexFormat vertexFormat = getVertexFormat(vertexAttribute.dataType, vertexAttribute.normalized);
+
+                if (vertexFormat == MTLVertexFormatInvalid)
+                {
+                    Log(Log::Level::ERR) << "Invalid vertex format";
+                    return false;
+                }
+
+                vertexDescriptor.attributes[index].format = vertexFormat;
+                vertexDescriptor.attributes[index].offset = offset;
+                vertexDescriptor.attributes[index].bufferIndex = 0;
+                ++index;
+                offset += getDataTypeSize(vertexAttribute.dataType);
+            }
+
+            vertexDescriptor.layouts[0].stride = offset;
+            vertexDescriptor.layouts[0].stepRate = 1;
+            vertexDescriptor.layouts[0].stepFunction = MTLVertexStepFunctionPerVertex;
+
+            NSError* err = Nil;
+
+            dispatch_data_t pixelShaderDispatchData = dispatch_data_create(pixelShaderData.data(), pixelShaderData.size(), NULL, DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+            id<MTLLibrary> pixelShaderLibrary = [rendererMetal->getDevice() newLibraryWithData:pixelShaderDispatchData error:&err];
+            dispatch_release(pixelShaderDispatchData);
+
+            if (!pixelShaderLibrary || err != Nil)
+            {
+                if (pixelShaderLibrary) [pixelShaderLibrary release];
+                Log(Log::Level::ERR) << "Failed to load pixel shader, " << (err ? [err.localizedDescription cStringUsingEncoding:NSUTF8StringEncoding] : "unknown error");
+                return false;
+            }
+
+            pixelShader = [pixelShaderLibrary newFunctionWithName:static_cast<NSString* _Nonnull>([NSString stringWithUTF8String:pixelShaderFunction.c_str()])];
+
+            [pixelShaderLibrary release];
+
+            if (!pixelShader || err != Nil)
+            {
+                Log(Log::Level::ERR) << "Failed to get function from shader, " << (err ? [err.localizedDescription cStringUsingEncoding:NSUTF8StringEncoding] : "unknown error");
+                return false;
+            }
+
+            if (!pixelShaderConstantInfo.empty())
+            {
+                pixelShaderConstantLocations.clear();
+                pixelShaderConstantLocations.reserve(pixelShaderConstantInfo.size());
+
+                pixelShaderConstantSize = 0;
+
+                for (const Shader::ConstantInfo& info : pixelShaderConstantInfo)
+                {
+                    pixelShaderConstantLocations.push_back({pixelShaderConstantSize, info.size});
+                    pixelShaderConstantSize += info.size;
+                }
+            }
+
+            dispatch_data_t vertexShaderDispatchData = dispatch_data_create(vertexShaderData.data(), vertexShaderData.size(), NULL, DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+            id<MTLLibrary> vertexShaderLibrary = [rendererMetal->getDevice() newLibraryWithData:vertexShaderDispatchData error:&err];
+            dispatch_release(vertexShaderDispatchData);
+
+            if (!vertexShaderLibrary || err != Nil)
+            {
+                if (vertexShaderLibrary) [vertexShaderLibrary release];
+                Log(Log::Level::ERR) << "Failed to load vertex shader, " << (err ? [err.localizedDescription cStringUsingEncoding:NSUTF8StringEncoding] : "unknown error");
+                return false;
+            }
+
+            vertexShader = [vertexShaderLibrary newFunctionWithName:static_cast<NSString* _Nonnull>([NSString stringWithUTF8String:vertexShaderFunction.c_str()])];
+
+            [vertexShaderLibrary release];
+
+            if (!vertexShader || err != Nil)
+            {
+                Log(Log::Level::ERR) << "Failed to get function from shader, " << (err ?[err.localizedDescription cStringUsingEncoding:NSUTF8StringEncoding] : "unknown error");
+                return false;
+            }
+
+            if (!vertexShaderConstantInfo.empty())
+            {
+                vertexShaderConstantLocations.clear();
+                vertexShaderConstantLocations.reserve(vertexShaderConstantInfo.size());
+
+                vertexShaderConstantSize = 0;
+
+                for (const Shader::ConstantInfo& info : vertexShaderConstantInfo)
+                {
+                    vertexShaderConstantLocations.push_back({vertexShaderConstantSize, info.size});
+                    vertexShaderConstantSize += info.size;
+                }
+            }
+
             return true;
         }
 
