f5d8857d05bb3989f47c76eeaffb33856569ceb9
Elviss Strazdins <elviss@elviss.lv>
2017-08-15 18:49:55 +0300

    Add attributes to Material

--- ouzel/graphics/Material.hpp
+++ ouzel/graphics/Material.hpp
@@ -3,13 +3,22 @@
 
 #pragma once
 
+#include <memory>
+#include "utils/Noncopyable.hpp"
+
 namespace ouzel
 {
     namespace graphics
     {
-        class Material
+        class BlendState;
+        class Texture;
+
+        class Material: public Noncopyable
         {
         public:
+            std::shared_ptr<BlendState> blendState;
+            std::shared_ptr<Texture> texture;
+            bool twoSided = false;
         };
     } // namespace graphics
 } // namespace ouzel
