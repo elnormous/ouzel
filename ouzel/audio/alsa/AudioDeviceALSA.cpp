12fa98a75b0d1b54e36e73ed16c1905773d439d1
Elviss Strazdins <elviss@elviss.lv>
2017-08-29 20:32:23 +0300

    Update audio after wait to reduce latency for ALSA backend

--- ouzel/audio/alsa/AudioDeviceALSA.cpp
+++ ouzel/audio/alsa/AudioDeviceALSA.cpp
@@ -131,11 +131,6 @@ namespace ouzel
         {
             while (running)
             {
-                if (!update())
-                {
-                    break;
-                }
-
                 int err;
 
                 if ((err = snd_pcm_wait(playbackHandle, 1000)) < 0)
@@ -155,6 +150,11 @@ namespace ouzel
                         Log(Log::Level::ERR) << "Failed to poll, error: " << err;
                         break;
                     }
+                }
+
+                if (!update())
+                {
+                    break;
                 }
 
                 snd_pcm_sframes_t frames;
