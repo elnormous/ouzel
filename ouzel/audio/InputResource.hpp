e4c818a5a7ae94d956a365fd19abeaf6cb1632eb
Elviss Strazdins <elviss@elviss.lv>
2017-09-04 23:06:08 +0300

    Add classes for audio Input

---
+++ ouzel/audio/InputResource.hpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2017 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#pragma once
+
+namespace ouzel
+{
+    namespace audio
+    {
+        class InputResource
+        {
+        public:
+        };
+    } // namespace audio
+} // namespace ouzel
