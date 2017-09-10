42a365e7496dd6f0f547b2371ad5e2305182302e
Elviss Strazdins <elviss@elviss.lv>
2017-09-11 00:03:29 +0300

    Fix channel volume calculation

--- ouzel/audio/Sound.cpp
+++ ouzel/audio/Sound.cpp
@@ -434,8 +434,8 @@ namespace ouzel
                     float angle = atan2f(relative.x, relative.z);
 
                     // constant power panning
-                    channelVolume[0] = SQRT2 / 2.0f * (cosf(angle) - sinf(angle));
-                    channelVolume[1] = SQRT2 / 2.0f * (cosf(angle) + sinf(angle));
+                    channelVolume[0] *= SQRT2 / 2.0f * (cosf(angle) - sinf(angle));
+                    channelVolume[1] *= SQRT2 / 2.0f * (cosf(angle) + sinf(angle));
                 }
 
                 for (uint32_t frame = 0; frame < result.size() / channels; ++frame)
