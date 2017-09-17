6680315d7c7c897f0c2c1c88d9d673fde61549f7
Elviss Strazdins <elviss@elviss.lv>
2017-09-17 13:06:08 +0300

    Implement Winsock initialization

--- ouzel/network/Network.cpp
+++ ouzel/network/Network.cpp
@@ -1,7 +1,16 @@
 // Copyright (C) 2017 Elviss Strazdins
 // This file is part of the Ouzel engine.
 
+#ifdef _WIN32
+#define NOMINMAX
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <poll.h>
+#endif
 #include "Network.hpp"
+#include "utils/Log.hpp"
 
 namespace ouzel
 {
@@ -13,10 +22,27 @@ namespace ouzel
 
         Network::~Network()
         {
+            WSACleanup();
         }
 
         bool Network::init()
         {
+            WORD sockVersion = MAKEWORD(2, 2);
+            WSADATA wsaData;
+            int error = WSAStartup(sockVersion, &wsaData);
+            if (error != 0)
+            {
+                Log(Log::Level::ERR) << "Failed to start Winsock failed, error: " << error;
+                return false;
+            }
+    
+            if (wsaData.wVersion != sockVersion)
+            {
+                Log(Log::Level::ERR) << "Incorrect Winsock version";
+                WSACleanup();
+                return false;
+            }
+
             return true;
         }
     } // namespace network
