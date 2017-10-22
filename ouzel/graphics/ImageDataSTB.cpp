45bcf1f735866f58d7ea3945108604bc676b292e
Elviss Strazdins <elviss@elviss.lv>
2017-10-22 21:47:12 +0300

    Disable psd, hdr, pic, gif, and pnm image formats

--- ouzel/graphics/ImageDataSTB.cpp
+++ ouzel/graphics/ImageDataSTB.cpp
@@ -5,6 +5,11 @@
 #include "utils/Log.hpp"
 #include "core/Engine.hpp"
 #include "files/FileSystem.hpp"
+#define STBI_NO_PSD
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_GIF
+#define STBI_NO_PNM
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
