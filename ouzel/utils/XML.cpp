b14030e93014aaa1814e1d2e11c4c666439f3f98
Elviss Strazdins <elviss@elviss.lv>
2017-11-23 00:39:32 +0200

    Move whitespace check to separate function

--- ouzel/utils/XML.cpp
+++ ouzel/utils/XML.cpp
@@ -420,6 +420,35 @@ namespace ouzel
             return true;
         }
 
+        static bool isWhitespace(uint32_t c)
+        {
+            return c == ' ' || c == '\t' || c == '\r' || c == '\n';
+        }
+
+        static bool isNameStartChar(uint32_t c)
+        {
+            return (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            c == '_' ||
+            (c >= 0xC0 && c <= 0xD6) ||
+            (c >= 0xD8 && c <= 0xF6) ||
+            (c >= 0xF8 && c <= 0x2FF) ||
+            (c >= 0x370 && c <= 0x37D) ||
+            (c >= 0x37F && c <= 0x1FFF) ||
+            (c >= 0x200C && c <= 0x200D) ||
+            (c >= 0x2070 && c <= 0x218F);
+        }
+
+        static bool isNameChar(uint32_t c)
+        {
+            return isNameStartChar(c) ||
+            c == '-' || c == '.' ||
+            (c >= '0' && c <= '9') ||
+            c == 0xB7 ||
+            (c >= 0x0300 && c <= 0x036F) ||
+            (c >= 0x203F && c <= 0x2040);
+        }
+        
         bool Node::skipWhitespaces(const std::vector<uint32_t>& utf32,
                                    std::vector<uint32_t>::iterator& iterator)
         {
@@ -431,7 +460,7 @@ namespace ouzel
                     return false;
                 }
 
-                if (*iterator == ' ' || *iterator == '\r' || *iterator == '\n' || *iterator == '\t')
+                if (isWhitespace(*iterator))
                 {
                     ++iterator;
                 }
@@ -442,30 +471,6 @@ namespace ouzel
             }
 
             return true;
-        }
-
-        static bool isNameStartChar(uint32_t c)
-        {
-            return (c >= 'a' && c <= 'z') ||
-                (c >= 'A' && c <= 'Z') ||
-                c == '_' ||
-                (c >= 0xC0 && c <= 0xD6) ||
-                (c >= 0xD8 && c <= 0xF6) ||
-                (c >= 0xF8 && c <= 0x2FF) ||
-                (c >= 0x370 && c <= 0x37D) ||
-                (c >= 0x37F && c <= 0x1FFF) ||
-                (c >= 0x200C && c <= 0x200D) ||
-                (c >= 0x2070 && c <= 0x218F);
-        }
-
-        static bool isNameChar(uint32_t c)
-        {
-            return isNameStartChar(c) ||
-                c == '-' || c == '.' ||
-                (c >= '0' && c <= '9') ||
-                c == 0xB7 ||
-                (c >= 0x0300 && c <= 0x036F) ||
-                (c >= 0x203F && c <= 0x2040);
         }
 
         bool Node::parseName(const std::vector<uint32_t>& utf32,
