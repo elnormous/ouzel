fc6cf00609eb31550fd41874a3c7cd876eb81f27
Elviss Strazdins <elviss@elviss.lv>
2018-02-13 02:17:51 +0200

    Add a workaround for older version of clang for macOS that don't support clock_gettime

--- ouzel/thread/Condition.cpp
+++ ouzel/thread/Condition.cpp
@@ -1,6 +1,10 @@
 // Copyright (C) 2018 Elviss Strazdins
 // This file is part of the Ouzel engine.
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
 #include "Condition.hpp"
 
 namespace ouzel
@@ -55,12 +59,24 @@ namespace ouzel
 #if defined(_MSC_VER)
         return SleepConditionVariableCS(&conditionVariable, &mutex.criticalSection, std::chrono::duration_cast<std::chrono::milliseconds>(duration).count());
 #else
-        static const long NSEC_PER_SEC = 1000000000L;
+        static const long NANOSEC_PER_SEC = 1000000000L;
         struct timespec ts;
+
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+        clock_serv_t cclock;
+        mach_timespec_t mts;
+        host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+        clock_get_time(cclock, &mts);
+        mach_port_deallocate(mach_task_self(), cclock);
+        ts.tv_sec = mts.tv_sec;
+        ts.tv_nsec = mts.tv_nsec;
+#else
         clock_gettime(CLOCK_REALTIME, &ts);
+#endif
+
         ts.tv_nsec += std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
 
-        ts.tv_sec += static_cast<int32_t>(ts.tv_nsec / NSEC_PER_SEC);
+        ts.tv_sec += static_cast<int32_t>(ts.tv_nsec / NANOSEC_PER_SEC);
         ts.tv_nsec %= NSEC_PER_SEC;
 
         return pthread_cond_timedwait(&condition, &mutex.mutex, &ts) == 0;
