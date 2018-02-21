d92b0f7e8b51237d619d64351f415f44617086f0
Elviss Strazdins <elviss@elviss.lv>
2018-02-22 00:59:37 +0200

    Start implementing the File class

--- ouzel/files/File.cpp
+++ ouzel/files/File.cpp
@@ -5,4 +5,52 @@
 
 namespace ouzel
 {
+    File::File()
+    {
+    }
+
+    File::File(const std::string& filename, int mode)
+    {
+        open(filename, mode);
+    }
+
+    File::~File()
+    {
+
+    }
+
+    File::File(File&& other)
+    {
+
+    }
+
+    File& File::operator=(File&& other)
+    {
+        return *this;
+    }
+
+    bool File::open(const std::string& filename, int mode)
+    {
+        return true;
+    }
+
+    bool File::close()
+    {
+        return true;
+    }
+
+    bool File::read(char* buffer, uint32_t& size)
+    {
+        return true;
+    }
+
+    bool File::write(const char* buffer, uint32_t& size)
+    {
+        return true;
+    }
+
+    bool File::seek(uint32_t position, int mode)
+    {
+        return true;
+    }
 }
--- ouzel/files/File.hpp
+++ ouzel/files/File.hpp
@@ -3,10 +3,43 @@
 
 #pragma once
 
+#include <string>
+
+#if OUZEL_PLATFORM_WINDOWS
+#include <windows.h>
+#else
+#endif
+
 namespace ouzel
 {
     class File
     {
     public:
+        File();
+        File(const std::string& filename, int mode);
+        ~File();
+
+        File(const File&) = delete;
+        File& operator=(const File&) = delete;
+
+        File(File&&);
+        File& operator=(File&&);
+
+        bool open(const std::string& filename, int mode);
+        bool close();
+
+        bool read(char* buffer, uint32_t& size);
+        bool write(const char* buffer, uint32_t& size);
+        bool seek(uint32_t position, int mode);
+
+    protected:
+#if OUZEL_PLATFORM_WINDOWS
+        HANDLE file = nullptr;
+#else
+        int fd = -1;
+#if OUZEL_PLATFORM_ANDROID
+        AAsset* asset = nullptr;
+#endif
+#endif
     };
 }
