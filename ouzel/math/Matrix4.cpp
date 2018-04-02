6f442c2e10ba8554f49fda66745906aff566a359
Elviss Strazdins <elviss@elviss.lv>
2018-04-02 16:27:55 +0300

    Don't check for isSufficientDelta twice in Matrix4::createBillboardHelper

--- ouzel/math/Matrix4.cpp
+++ ouzel/math/Matrix4.cpp
@@ -182,23 +182,20 @@ namespace ouzel
 
         // As per the contracts for the 2 variants of createBillboard, we need
         // either a safe default or a sufficient distance between object and camera
-        if (isSufficientDelta)
-        {
-            Vector3 target = isSufficientDelta ? cameraPosition : (objectPosition - cameraForwardVector);
+        Vector3 target = isSufficientDelta ? cameraPosition : (objectPosition - cameraForwardVector);
 
-            // A billboard is the inverse of a lookAt rotation
-            Matrix4 lookAt;
-            createLookAt(objectPosition, target, cameraUpVector, lookAt);
-            dst.m[0] = lookAt.m[0];
-            dst.m[1] = lookAt.m[4];
-            dst.m[2] = lookAt.m[8];
-            dst.m[4] = lookAt.m[1];
-            dst.m[5] = lookAt.m[5];
-            dst.m[6] = lookAt.m[9];
-            dst.m[8] = lookAt.m[2];
-            dst.m[9] = lookAt.m[6];
-            dst.m[10] = lookAt.m[10];
-        }
+        // A billboard is the inverse of a lookAt rotation
+        Matrix4 lookAt;
+        createLookAt(objectPosition, target, cameraUpVector, lookAt);
+        dst.m[0] = lookAt.m[0];
+        dst.m[1] = lookAt.m[4];
+        dst.m[2] = lookAt.m[8];
+        dst.m[4] = lookAt.m[1];
+        dst.m[5] = lookAt.m[5];
+        dst.m[6] = lookAt.m[9];
+        dst.m[8] = lookAt.m[2];
+        dst.m[9] = lookAt.m[6];
+        dst.m[10] = lookAt.m[10];
     }
 
     void Matrix4::createScale(const Vector3& scale, Matrix4& dst)
