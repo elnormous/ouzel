39e1a54ecb0c1c74856a44f9107402c1254df427
Elviss Strazdins <elviss@elviss.lv>
2018-04-03 00:27:08 +0300

    Make Lock's constructor explicit

--- ouzel/thread/Lock.hpp
+++ ouzel/thread/Lock.hpp
@@ -10,7 +10,7 @@ namespace ouzel
     class Lock final
     {
     public:
-        Lock(Mutex& initMutex):
+        explicit Lock(Mutex& initMutex):
             mutex(&initMutex)
         {
             mutex->lock();
