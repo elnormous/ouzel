6c150309e33242a2626e97f2155aef5caaaa7034
Elviss Strazdins <elviss@elviss.lv>
2018-05-03 21:58:35 +0300

    Create InputSystem class

---
+++ ouzel/input/InputSystem.hpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2018 Elviss Strazdins
+// This file is part of the Ouzel engine.
+
+#pragma once
+
+namespace ouzel
+{
+    namespace input
+    {
+        class InputSystem
+        {
+        public:
+        };
+    } // namespace input
+} // namespace ouzel
