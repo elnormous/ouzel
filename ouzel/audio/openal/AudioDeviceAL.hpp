8bdaea710e4a395e9edb9aed889e576285c940b7
Elviss Strazdins <elviss@elviss.lv>
2018-05-20 01:31:39 +0300

    Remove the logError parameter from OpenGL and OpenAL error checkers Inline OpenAL error checkers

--- ouzel/audio/openal/AudioDeviceAL.hpp
+++ ouzel/audio/openal/AudioDeviceAL.hpp
@@ -20,6 +20,7 @@
 
 #include "audio/AudioDevice.hpp"
 #include "thread/Thread.hpp"
+#include "utils/Log.hpp"
 
 namespace ouzel
 {
@@ -29,8 +30,57 @@ namespace ouzel
         {
             friend Audio;
         public:
-            bool checkALCError(bool logError = true);
-            static bool checkOpenALError(bool logError = true);
+            static inline bool checkOpenALError()
+            {
+                ALenum error = alGetError();
+
+                if (error != AL_NO_ERROR)
+                {
+                    const char* errorStr;
+
+                    switch (error)
+                    {
+                        case AL_INVALID_NAME: errorStr = "AL_INVALID_NAME"; break;
+                        case AL_INVALID_ENUM: errorStr = "AL_INVALID_ENUM"; break;
+                        case AL_INVALID_VALUE: errorStr = "AL_INVALID_VALUE"; break;
+                        case AL_INVALID_OPERATION: errorStr = "AL_INVALID_OPERATION"; break;
+                        case AL_OUT_OF_MEMORY: errorStr = "AL_OUT_OF_MEMORY"; break;
+                        default: errorStr = "Unknown error"; break;
+                    }
+
+                    Log(Log::Level::ERR) << "OpenAL error: " << errorStr << " (" << error << ")";
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            inline bool checkALCError()
+            {
+                ALCenum error = alcGetError(device);
+
+                if (error != ALC_NO_ERROR)
+                {
+                    const char* errorStr;
+
+                    switch (error)
+                    {
+                        case ALC_INVALID_DEVICE: errorStr = "ALC_INVALID_DEVICE"; break;
+                        case ALC_INVALID_CONTEXT: errorStr = "ALC_INVALID_CONTEXT"; break;
+                        case ALC_INVALID_ENUM: errorStr = "ALC_INVALID_ENUM"; break;
+                        case ALC_INVALID_VALUE: errorStr = "ALC_INVALID_VALUE"; break;
+                        case ALC_OUT_OF_MEMORY: errorStr = "ALC_OUT_OF_MEMORY"; break;
+                        default: errorStr = "Unknown error"; break;
+                    }
+
+                    Log(Log::Level::ERR) << "OpenAL error: " << errorStr << "(" << error << ")";
+
+                    return true;
+                }
+
+                return false;
+            }
 
             virtual ~AudioDeviceAL();
 
