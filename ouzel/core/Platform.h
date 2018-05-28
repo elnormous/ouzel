a6ec12e91095d63ddd202c081a8d7abb29ff22a6
Elviss Strazdins <elviss@elviss.lv>
2018-05-28 23:09:48 +0300

    Add defines for endianness

--- ouzel/core/Platform.h
+++ ouzel/core/Platform.h
@@ -3,6 +3,54 @@
 
 #pragma once
 
+#if defined (__GLIBC__)
+# include <endian.h>
+# if (__BYTE_ORDER == __LITTLE_ENDIAN)
+#  define OUZEL_LITTLE_ENDIAN
+# elif (__BYTE_ORDER == __BIG_ENDIAN)
+#  define OUZEL_BIG_ENDIAN
+# else
+#  error Unsupported endianness
+# endif
+
+#elif defined(__ANDROID__)
+# include "machine/_types.h"
+# ifdef __ARMEB__
+#  define OUZEL_BIG_ENDIAN
+# else
+#  define OUZEL_LITTLE_ENDIAN
+# endif
+
+#elif defined(_BIG_ENDIAN) && !defined(_LITTLE_ENDIAN) || \
+defined(__BIG_ENDIAN__) && !defined(__LITTLE_ENDIAN__) || \
+defined(__BIGENDIAN__) && !defined(__LITTLEENDIAN__) || \
+defined(_STLP_BIG_ENDIAN) && !defined(_STLP_LITTLE_ENDIAN)
+# define OUZEL_BIG_ENDIAN
+#elif defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN) || \
+defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__) || \
+defined(__LITTLEENDIAN__) && !defined(__BIGENDIAN__) || \
+defined(_STLP_LITTLE_ENDIAN) && !defined(_STLP_BIG_ENDIAN)
+# define OUZEL_LITTLE_ENDIAN
+#elif defined(__sparc) || defined(__sparc__) \
+|| defined(_POWER) || defined(__powerpc__) \
+|| defined(__ppc__) || defined(__hpux) || defined(__hppa) \
+|| defined(_MIPSEB) || defined(_POWER) \
+|| defined(__s390__) || defined(__ARMEB__)
+# define OUZEL_BIG_ENDIAN
+#elif defined(__i386__) || defined(__alpha__) \
+|| defined(__ia64) || defined(__ia64__) \
+|| defined(_M_IX86) || defined(_M_IA64) \
+|| defined(_M_ALPHA) || defined(__amd64) \
+|| defined(__amd64__) || defined(_M_AMD64) \
+|| defined(__x86_64) || defined(__x86_64__) \
+|| defined(_M_X64) || defined(__bfin__) \
+|| defined(__ARMEL__) \
+|| (defined(_WIN32) && defined(__ARM__) && defined(_MSC_VER))
+# define OUZEL_LITTLE_ENDIAN
+#else
+# error Unsupported endianness
+#endif
+
 #if defined(_WIN32) // Windows
 #  define OUZEL_PLATFORM_WINDOWS 1
 #  define OUZEL_SUPPORTS_DIRECT3D 1
@@ -135,6 +183,9 @@
 #  define OUZEL_SUPPORTS_OPENGLES 1
 #  define OUZEL_OPENGL_INTERFACE_EGL 1
 #  define OUZEL_SUPPORTS_OPENAL 1
+
+#else
+# error "Unsupported platform"
 #endif
 
 #if defined(__SSE__)
