3ce59ac6e9bb811164de06aaefd932237aeefb62
Elviss Strazdins <elviss@elviss.lv>
2018-06-03 20:55:13 +0300

    Add flags to the render target

--- ouzel/graphics/RenderTargetResource.hpp
+++ ouzel/graphics/RenderTargetResource.hpp
@@ -12,6 +12,13 @@ namespace ouzel
         class RenderTargetResource: public RenderResource
         {
         public:
+            enum Flags
+            {
+                COLOR_BUFFER = 0x01,
+                DEPTH_BUFFER = 0x02,
+                BINDABLE_COLOR_BUFFER = 0x04,
+                BINDABLE_DEPTH_BUFFER = 0x08,
+            };
         };
     } // namespace graphics
 } // namespace ouzel
