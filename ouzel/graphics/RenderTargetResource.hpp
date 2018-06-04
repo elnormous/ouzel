abb5c3ca9b46ab5a8178c0f2592247994f19acba
Elviss Strazdins <elviss@elviss.lv>
2018-06-05 01:42:52 +0300

    Add color and depth textures to render target resource

--- ouzel/graphics/RenderTargetResource.hpp
+++ ouzel/graphics/RenderTargetResource.hpp
@@ -9,6 +9,8 @@ namespace ouzel
 {
     namespace graphics
     {
+        class TextureResource;
+
         class RenderTargetResource: public RenderResource
         {
         public:
@@ -19,6 +21,13 @@ namespace ouzel
                 BINDABLE_COLOR_BUFFER = 0x04,
                 BINDABLE_DEPTH_BUFFER = 0x08,
             };
+
+            TextureResource* getColorTexture() const { return colorTexture; }
+            TextureResource* getDepthTexture() const { return depthTexture; }
+
+        private:
+            TextureResource* colorTexture = nullptr;
+            TextureResource* depthTexture = nullptr;
         };
     } // namespace graphics
 } // namespace ouzel
