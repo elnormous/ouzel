18e56c3b8be35339ee940369be964ac67fdb7afa
Elviss Strazdins <elviss@elviss.lv>
2018-06-05 01:45:54 +0300

    Move flags to RenderTarget and add color and depth textures to it

--- ouzel/graphics/RenderTarget.hpp
+++ ouzel/graphics/RenderTarget.hpp
@@ -7,9 +7,25 @@ namespace ouzel
 {
     namespace graphics
     {
+        class Texture;
+        
         class RenderTarget final
         {
         public:
+            enum Flags
+            {
+                COLOR_BUFFER = 0x01,
+                DEPTH_BUFFER = 0x02,
+                BINDABLE_COLOR_BUFFER = 0x04,
+                BINDABLE_DEPTH_BUFFER = 0x08,
+            };
+
+            Texture* getColorTexture() const { return colorTexture; }
+            Texture* getDepthTexture() const { return depthTexture; }
+
+        private:
+            Texture* colorTexture = nullptr;
+            Texture* depthTexture = nullptr;
         };
     } // namespace graphics
 } // namespace ouzel
--- ouzel/graphics/RenderTargetResource.hpp
+++ ouzel/graphics/RenderTargetResource.hpp
@@ -14,14 +14,6 @@ namespace ouzel
         class RenderTargetResource: public RenderResource
         {
         public:
-            enum Flags
-            {
-                COLOR_BUFFER = 0x01,
-                DEPTH_BUFFER = 0x02,
-                BINDABLE_COLOR_BUFFER = 0x04,
-                BINDABLE_DEPTH_BUFFER = 0x08,
-            };
-
             TextureResource* getColorTexture() const { return colorTexture; }
             TextureResource* getDepthTexture() const { return depthTexture; }
 
