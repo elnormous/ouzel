ab081961ddbfd4b83f04028ae1ec44f1f2d992f2
Elviss Strazdins <elviss@elviss.lv>
2018-06-25 00:47:33 +0300

    Fix the return type of the WindowResourceTypeEm::init method

--- ouzel/core/emscripten/WindowResourceEm.hpp
+++ ouzel/core/emscripten/WindowResourceEm.hpp
@@ -19,7 +19,7 @@ namespace ouzel
 
     protected:
         WindowResourceEm();
-        virtual bool init(const Size2& newSize,
+        virtual void init(const Size2& newSize,
                           bool newResizable,
                           bool newFullscreen,
                           bool newExclusiveFullscreen,
