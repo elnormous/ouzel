8936d5634a37f13757720bda5de67d632d1c352d
Elviss Strazdins <elviss@elviss.lv>
2018-07-04 12:43:36 +0300

    Add color, direction and range attributes to Light class

--- ouzel/scene/Light.hpp
+++ ouzel/scene/Light.hpp
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "scene/Component.hpp"
+#include "math/Color.hpp"
+#include "math/Quaternion.hpp"
 
 namespace ouzel
 {
@@ -23,11 +25,23 @@ namespace ouzel
             Light(Type initType);
             virtual ~Light();
 
+            Color getColor() const { return color; }
+            void setColor(Color newColor) { color = newColor; }
+
+            const Quaternion& getDirection() const { return direction; }
+            void setDirection(const Quaternion& newDirection) { direction = newDirection; }
+
+            float getRange() const { return range; }
+            void setRange(float newRange) { range = newRange; }
+
         protected:
             virtual void setLayer(Layer* newLayer) override;
 
         private:
             Type type;
+            Color color;
+            Quaternion direction = Quaternion::identity();
+            float range = 0.0F;
         };
     } // namespace scene
 } // namespace ouzel
