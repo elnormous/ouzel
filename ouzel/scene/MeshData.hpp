4bd9ef49428fd302fdc76289ab33dc124805ff3b
Elviss Strazdins <elviss@elviss.lv>
2018-07-16 01:06:46 +0300

    Remove init method from the MeshData and move it to constructor

--- ouzel/scene/MeshData.hpp
+++ ouzel/scene/MeshData.hpp
@@ -15,15 +15,16 @@ namespace ouzel
         class MeshData
         {
         public:
-            void init(Box3 newBoundingBox,
-                      const std::vector<uint32_t> indices,
-                      const std::vector<graphics::Vertex>& vertices,
-                      const std::shared_ptr<graphics::Material>& newMaterial);
+            MeshData() {}
+            MeshData(Box3 newBoundingBox,
+                     const std::vector<uint32_t> indices,
+                     const std::vector<graphics::Vertex>& vertices,
+                     const std::shared_ptr<graphics::Material>& newMaterial);
 
             Box3 boundingBox;
             std::shared_ptr<graphics::Material> material;
-            uint32_t indexCount;
-            uint32_t indexSize;
+            uint32_t indexCount = 0;
+            uint32_t indexSize = 0;
             std::shared_ptr<graphics::Buffer> indexBuffer;
             std::shared_ptr<graphics::Buffer> vertexBuffer;
         };
