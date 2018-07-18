63c2f90b295689cc9a66b540661c9135de12c86d
Elviss Strazdins <elviss@elviss.lv>
2018-07-18 20:48:31 +0300

    Remove the init method from Shader that initializes the shader from files

--- ouzel/graphics/Shader.cpp
+++ ouzel/graphics/Shader.cpp
@@ -4,8 +4,6 @@
 #include "ShaderResource.hpp"
 #include "Renderer.hpp"
 #include "RenderDevice.hpp"
-#include "core/Engine.hpp"
-#include "files/FileSystem.hpp"
 
 namespace ouzel
 {
@@ -20,38 +18,6 @@ namespace ouzel
         Shader::~Shader()
         {
             if (resource) renderer->getDevice()->deleteResource(resource);
-        }
-
-        void Shader::init(const std::string& newFragmentShader,
-                          const std::string& newVertexShader,
-                          const std::set<Vertex::Attribute::Usage>& newVertexAttributes,
-                          const std::vector<ConstantInfo>& newFragmentShaderConstantInfo,
-                          const std::vector<ConstantInfo>& newVertexShaderConstantInfo,
-                          uint32_t newFragmentShaderDataAlignment,
-                          uint32_t newVertexShaderDataAlignment,
-                          const std::string& newFragmentShaderFunction,
-                          const std::string& newVertexShaderFunction)
-        {
-            vertexAttributes = newVertexAttributes;
-
-            fragmentShaderFilename = newFragmentShader;
-            vertexShaderFilename = newVertexShader;
-
-            std::vector<uint8_t> fragmentShaderData = engine->getFileSystem()->readFile(newFragmentShader);
-            std::vector<uint8_t> vertexShaderData = engine->getFileSystem()->readFile(newVertexShader);
-
-            RenderDevice* renderDevice = renderer->getDevice();
-
-            renderDevice->addCommand(InitShaderCommand(resource,
-                                                       fragmentShaderData,
-                                                       vertexShaderData,
-                                                       newVertexAttributes,
-                                                       newFragmentShaderConstantInfo,
-                                                       newVertexShaderConstantInfo,
-                                                       newFragmentShaderDataAlignment,
-                                                       newVertexShaderDataAlignment,
-                                                       newFragmentShaderFunction,
-                                                       newVertexShaderFunction));
         }
 
         void Shader::init(const std::vector<uint8_t>& newFragmentShader,
