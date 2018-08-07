8f87c3e7b47ec653cd21dfc993b5f04859ff6845
Elviss Strazdins <elviss@elviss.lv>
2018-08-08 00:57:53 +0300

    Move socket to Socket class

--- ouzel/network/Client.cpp
+++ ouzel/network/Client.cpp
@@ -1,17 +1,6 @@
 // Copyright 2015-2018 Elviss Strazdins. All rights reserved.
 
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#else
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <unistd.h>
-#endif
+#include <utility>
 #include "Client.hpp"
 
 namespace ouzel
@@ -20,41 +9,18 @@ namespace ouzel
     {
         Client::~Client()
         {
-#ifdef _WIN32
-            if (endpoint != INVALID_SOCKET) closesocket(endpoint);
-#else
-            if (endpoint != -1) close(endpoint);
-#endif
         }
 
         Client::Client(Client&& other)
         {
-            endpoint = other.endpoint;
-
-#ifdef _WIN32
-            other.endpoint = INVALID_SOCKET;
-#else
-            other.endpoint = -1;
-#endif
+            sock = std::move(other.sock);
         }
 
         Client& Client::operator=(Client&& other)
         {
             if (&other != this)
             {
-#ifdef _WIN32
-                if (endpoint != INVALID_SOCKET) closesocket(endpoint);
-#else
-                if (endpoint != -1) close(endpoint);
-#endif
-
-                endpoint = other.endpoint;
-
-#ifdef _WIN32
-                other.endpoint = INVALID_SOCKET;
-#else
-                other.endpoint = -1;
-#endif
+                sock = std::move(other.sock);
             }
 
             return *this;
