975f48ead2c95e67e531afa8ec553829f8220ba7
Elviss Strazdins <elviss@elviss.lv>
2018-09-12 16:29:52 +0300

    Add update method to InputManager and start implementing input map

--- ouzel/core/Engine.cpp
+++ ouzel/core/Engine.cpp
@@ -649,6 +649,7 @@ namespace ouzel
     {
         eventDispatcher.dispatchEvents();
 
+        inputManager->update();
         window->update();
         sceneManager.update();
 
---
+++ ouzel/input/Input.hpp
@@ -0,0 +1,13 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#pragma once
+
+namespace ouzel
+{
+    namespace input
+    {
+        class Input final
+        {
+        };
+    }
+}
