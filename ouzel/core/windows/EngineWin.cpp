a81ac9990b4f8f1b2a18737e0535f6410873dd51
Elviss Strazdins <elviss@elviss.lv>
2018-09-12 14:50:56 +0300

    Remove unused struct

--- ouzel/core/windows/EngineWin.cpp
+++ ouzel/core/windows/EngineWin.cpp
@@ -8,17 +8,6 @@
 #include "utils/Errors.hpp"
 #include "utils/Log.hpp"
 
-const DWORD MS_VC_EXCEPTION = 0x406D1388;
-#pragma pack(push,8)
-typedef struct tagTHREADNAME_INFO
-{
-    DWORD dwType; // Must be 0x1000.
-    LPCSTR szName; // Pointer to name (in user addr space).
-    DWORD dwThreadID; // Thread ID (-1=caller thread).
-    DWORD dwFlags; // Reserved for future use, must be zero.
-} THREADNAME_INFO;
-#pragma pack(pop)
-
 namespace ouzel
 {
     EngineWin::EngineWin(int initArgc, LPWSTR* initArgv)
