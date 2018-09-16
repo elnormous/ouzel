7ae3a90a446cfe6eac792bf9028943738261852a
Elviss Strazdins <elviss@elviss.lv>
2018-09-16 21:11:29 +0300

    Add parameters to blit command

--- ouzel/graphics/opengl/RenderDeviceOGL.hpp
+++ ouzel/graphics/opengl/RenderDeviceOGL.hpp
@@ -104,12 +104,12 @@ extern PFNGLMAPBUFFEROESPROC glMapBufferProc;
 extern PFNGLUNMAPBUFFEROESPROC glUnmapBufferProc;
 extern PFNGLMAPBUFFERRANGEEXTPROC glMapBufferRangeProc;
 extern PFNGLFRAMEBUFFERTEXTURE2DMULTISAMPLEEXTPROC glFramebufferTexture2DMultisampleProc;
-extern PFNGLCOPYIMAGESUBDATAEXTPROC glCopyImageSubDataProc;
+extern PFNGLCOPYIMAGESUBDATAOESPROC glCopyImageSubDataProc;
 #else
 extern PFNGLMAPBUFFERPROC glMapBufferProc;
 extern PFNGLUNMAPBUFFERPROC glUnmapBufferProc;
 extern PFNGLMAPBUFFERRANGEPROC glMapBufferRangeProc;
-extern PFNGLCOPYIMAGESUBDATAOESPROC glCopyImageSubDataProc;
+extern PFNGLCOPYIMAGESUBDATAEXTPROC glCopyImageSubDataProc;
 #endif
 
 #include "graphics/RenderDevice.hpp"
