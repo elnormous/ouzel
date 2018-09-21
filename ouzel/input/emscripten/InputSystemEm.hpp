dafe9dd0705320d1e294f078cdd530874636a27f
Elviss Strazdins <elviss@elviss.lv>
2018-09-21 03:36:45 +0300

    Add platform-specific subclasses of InputSystem

---
+++ ouzel/input/emscripten/InputSystemEm.hpp
@@ -0,0 +1,17 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#pragma once
+
+#include "input/InputSystem.hpp"
+
+namespace ouzel
+{
+    namespace input
+    {
+        class InputSystemEm: public InputSystem
+        {
+        public:
+            virtual ~InputSystemEm() {}
+        };
+    }
+}
