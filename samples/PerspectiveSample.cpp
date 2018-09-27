453d20a5e8b6328894f8bdd2e867a94ff9b3e6dd
Elviss Strazdins <elviss@elviss.lv>
2018-09-28 00:26:27 +0300

    Fix the samples

--- samples/PerspectiveSample.cpp
+++ samples/PerspectiveSample.cpp
@@ -179,7 +179,7 @@ bool PerspectiveSample::handleGamepad(Event::Type type, const GamepadEvent& even
     if (type == Event::Type::GAMEPAD_BUTTON_CHANGE)
     {
         if (event.pressed &&
-            event.button == input::Gamepad::Button::FACE_RIGHT)
+            event.button == input::Gamepad::Button::FACE_3)
             engine->getSceneManager().setScene(std::unique_ptr<scene::Scene>(new MainMenu()));
     }
 
