f27578e1ac1290f4cf8d117029c4c86c078c9edd
Elviss Strazdins <elviss@elviss.lv>
2018-10-06 04:54:38 +0300

    Handle shift key release on Windows

--- ouzel/input/windows/KeyboardDeviceWin.cpp
+++ ouzel/input/windows/KeyboardDeviceWin.cpp
@@ -1,19 +1,30 @@
 // Copyright 2015-2018 Elviss Strazdins. All rights reserved.
 
+#ifndef NOMINMAX
+#  define NOMINMAX
+#endif
+#include <Windows.h>
 #include "KeyboardDeviceWin.hpp"
 
 namespace ouzel
 {
     namespace input
     {
+        void KeyboardDeviceWin::handleKeyPress(Keyboard::Key key, uint32_t modifiers)
+        {
+            if (key == Keyboard::Key::LEFT_SHIFT) leftShiftDown = true;
+            if (key == Keyboard::Key::RIGHT_SHIFT) rightShiftDown = true;
+
+            KeyboardDevice::handleKeyPress(key, modifiers);
+        }
+
         void KeyboardDeviceWin::update()
         {
-            // TODO: implement
-            /*if (keyboardKeyStates[static_cast<uint32_t>(Keyboard::Key::LEFT_SHIFT)] && (GetKeyState(VK_LSHIFT) & 0x8000) == 0)
-                keyboardDevice->handleKeyRelease(Keyboard::Key::LEFT_SHIFT, 0);
+            if (leftShiftDown && (GetKeyState(VK_LSHIFT) & 0x8000) == 0)
+                handleKeyRelease(Keyboard::Key::LEFT_SHIFT, 0);
 
-            if (keyboardKeyStates[static_cast<uint32_t>(Keyboard::Key::RIGHT_SHIFT)] && (GetKeyState(VK_RSHIFT) & 0x8000) == 0)
-                keyboardDevice->handleKeyRelease(Keyboard::Key::RIGHT_SHIFT, 0);*/
+            if (rightShiftDown && (GetKeyState(VK_RSHIFT) & 0x8000) == 0)
+                handleKeyRelease(Keyboard::Key::RIGHT_SHIFT, 0);
         }
     } // namespace input
 } // namespace ouzel
