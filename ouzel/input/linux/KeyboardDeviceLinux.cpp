71b05f60be11308f6bec9201c92ca080b531f612
Elviss Strazdins <elviss@elviss.lv>
2018-10-07 04:01:18 +0300

    Add class for keyboard device on Linux

---
+++ ouzel/input/linux/KeyboardDeviceLinux.cpp
@@ -0,0 +1,11 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#include "core/Setup.h"
+#include "KeyboardDeviceLinux.hpp"
+
+namespace ouzel
+{
+    namespace input
+    {
+    } // namespace input
+} // namespace ouzel
---
+++ ouzel/input/linux/KeyboardDeviceLinux.hpp
@@ -0,0 +1,21 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#pragma once
+
+#include "input/KeyboardDevice.hpp"
+
+namespace ouzel
+{
+    namespace input
+    {
+        class KeyboardDeviceLinux: public KeyboardDevice
+        {
+        public:
+            KeyboardDeviceLinux(InputSystem& initInputSystem,
+                                uint32_t initId):
+                KeyboardDevice(initInputSystem, initId)
+            {
+            }
+        };
+    } // namespace input
+} // namespace ouzel
