212313c9d46f4bbad80d85d61eb3b20180be2d99
Elviss Strazdins <elviss@elviss.lv>
2018-11-22 03:44:59 +0200

    Add system class for each platform

---
+++ ouzel/core/tvos/SystemTVOS.hpp
@@ -0,0 +1,17 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#ifndef OUZEL_CORE_SYSTEMTVOS_HPP
+#define OUZEL_CORE_SYSTEMTVOS_HPP
+
+#include "core/System.hpp"
+
+namespace ouzel
+{
+    class SystemTVOS final: public System
+    {
+    public:
+        ~SystemTVOS() {}
+    };
+}
+
+#endif // OUZEL_CORE_SYSTEMTVOS_HPP
