26c69484c8436651e222b4d17436341129f44b2c
Elviss Strazdins <elviss@elviss.lv>
2018-11-22 03:36:51 +0200

    Add the new classes to the Visual Studio project

---
+++ ouzel/core/windows/SystemWin.hpp
@@ -0,0 +1,17 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#ifndef OUZEL_CORE_SYSTEMWIN_HPP
+#define OUZEL_CORE_SYSTEMWIN_HPP
+
+#include "core/System.hpp"
+
+namespace ouzel
+{
+    class SystemWin final: public System
+    {
+    public:
+        ~SystemWin() {}
+    };
+}
+
+#endif // OUZEL_CORE_SYSTEMWIN_HPP
