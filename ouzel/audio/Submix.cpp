e4f189047f2edd6b7fee36197f16b39f953159bb
Elviss Strazdins <elviss@elviss.lv>
2018-11-28 03:14:00 +0200

    Add Submix class

---
+++ ouzel/audio/Submix.cpp
@@ -0,0 +1,11 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#include "Submix.hpp"
+
+namespace ouzel
+{
+    namespace audio
+    {
+
+    } // namespace audio
+} // namespace ouzel
