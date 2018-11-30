4d615fb8e7251f747196b06709dcf88d2bd23a11
Elviss Strazdins <elviss@elviss.lv>
2018-12-01 01:42:19 +0200

    Create class for audio busses

---
+++ ouzel/audio/Bus.hpp
@@ -0,0 +1,17 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#ifndef OUZEL_AUDIO_BUS_HPP
+#define OUZEL_AUDIO_BUS_HPP
+
+namespace ouzel
+{
+    namespace audio
+    {
+        class Bus
+        {
+        public:
+        };
+    } // namespace audio
+} // namespace ouzel
+
+#endif //OUZEL_AUDIO_BUS_HPP
