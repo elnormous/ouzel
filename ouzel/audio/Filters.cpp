b6c9705efa2f1de5fa13ce6196a69472276429d8
Elviss Strazdins <elviss@elviss.lv>
2018-12-15 03:27:57 +0200

    Move all the filters to a single file

---
+++ ouzel/audio/Filters.cpp
@@ -0,0 +1,266 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#include <cmath>
+#include "Filters.hpp"
+#include "Audio.hpp"
+#include "scene/Actor.hpp"
+#include "math/MathUtils.hpp"
+#include "smbPitchShift.hpp"
+
+namespace ouzel
+{
+    namespace audio
+    {
+        class DelayProcessor final: public Processor
+        {
+        public:
+            DelayProcessor()
+            {
+            }
+
+            void process(uint32_t frames, uint16_t channels, uint32_t sampleRate,
+                         std::vector<float>& samples) override
+            {
+            }
+        };
+
+        Delay::Delay(Audio& initAudio):
+            Filter(initAudio,
+            initAudio.initProcessor(std::unique_ptr<Processor>(new DelayProcessor())))
+        {
+        }
+
+        Delay::~Delay()
+        {
+        }
+
+        void Delay::setDelay(float newDelay)
+        {
+            delay = newDelay;
+        }
+
+        class GainProcessor final: public Processor
+        {
+        public:
+            GainProcessor()
+            {
+            }
+
+            void process(uint32_t, uint16_t, uint32_t,
+                         std::vector<float>& samples) override
+            {
+                for (float& sample : samples)
+                    sample *= gainFactor;
+            }
+
+            void setGain(float newGain)
+            {
+                gain = newGain;
+                gainFactor = powf(10.0F, gain / 20.0F);
+            }
+
+        private:
+            float gain = 0.0F;
+            float gainFactor = 1.0F;
+        };
+
+        Gain::Gain(Audio& initAudio):
+            Filter(initAudio,
+            initAudio.initProcessor(std::unique_ptr<Processor>(new GainProcessor())))
+        {
+        }
+
+        Gain::~Gain()
+        {
+        }
+
+        void Gain::setGain(float newGain)
+        {
+            gain = newGain;
+
+            audio.updateProcessor(processorId, [newGain](Object* node) {
+                GainProcessor* gainProcessor = static_cast<GainProcessor*>(node);
+                gainProcessor->setGain(newGain);
+            });
+        }
+
+        class PannerProcessor final: public Processor
+        {
+        public:
+            PannerProcessor()
+            {
+            }
+
+            void process(uint32_t frames, uint16_t channels, uint32_t sampleRate,
+                         std::vector<float>& samples) override
+            {
+            }
+
+            void setPosition(const Vector3& newPosition)
+            {
+                position = newPosition;
+            }
+
+            void setRolloffFactor(float newRolloffFactor)
+            {
+                rolloffFactor = newRolloffFactor;
+            }
+
+            void setMinDistance(float newMinDistance)
+            {
+                minDistance = newMinDistance;
+            }
+
+            void setMaxDistance(float newMaxDistance)
+            {
+                maxDistance = newMaxDistance;
+            }
+
+        private:
+            Vector3 position;
+            float rolloffFactor = 1.0F;
+            float minDistance = 1.0F;
+            float maxDistance = FLT_MAX;
+        };
+
+        Panner::Panner(Audio& initAudio):
+            Filter(initAudio,
+                   initAudio.initProcessor(std::unique_ptr<Processor>(new PannerProcessor()))),
+            scene::Component(scene::Component::SOUND)
+        {
+        }
+
+        Panner::~Panner()
+        {
+        }
+
+        void Panner::setPosition(const Vector3& newPosition)
+        {
+            position = newPosition;
+
+            audio.updateProcessor(processorId, [newPosition](Object* node) {
+                PannerProcessor* pannerProcessor = static_cast<PannerProcessor*>(node);
+                pannerProcessor->setPosition(newPosition);
+            });
+        }
+
+        void Panner::setRolloffFactor(float newRolloffFactor)
+        {
+            rolloffFactor = newRolloffFactor;
+
+            audio.updateProcessor(processorId, [newRolloffFactor](Object* node) {
+                PannerProcessor* pannerProcessor = static_cast<PannerProcessor*>(node);
+                pannerProcessor->setRolloffFactor(newRolloffFactor);
+            });
+        }
+
+        void Panner::setMinDistance(float newMinDistance)
+        {
+            minDistance = newMinDistance;
+
+            audio.updateProcessor(processorId, [newMinDistance](Object* node) {
+                PannerProcessor* pannerProcessor = static_cast<PannerProcessor*>(node);
+                pannerProcessor->setMinDistance(newMinDistance);
+            });
+        }
+
+        void Panner::setMaxDistance(float newMaxDistance)
+        {
+            maxDistance = newMaxDistance;
+
+            audio.updateProcessor(processorId, [newMaxDistance](Object* node) {
+                PannerProcessor* pannerProcessor = static_cast<PannerProcessor*>(node);
+                pannerProcessor->setMaxDistance(newMaxDistance);
+            });
+        }
+
+        void Panner::updateTransform()
+        {
+            setPosition(actor->getWorldPosition());
+        }
+
+        static constexpr float MIN_PITCH = 0.5F;
+        static constexpr float MAX_PITCH = 2.0F;
+
+        class PitchProcessor final: public Processor
+        {
+        public:
+            PitchProcessor(float initPitch):
+                pitch(clamp(initPitch, MIN_PITCH, MAX_PITCH))
+            {
+            }
+
+            void process(uint32_t frames, uint16_t channels, uint32_t sampleRate,
+                         std::vector<float>& samples) override
+            {
+                channelSamples.resize(frames);
+                pitchShift.resize(channels);
+
+                for (uint16_t channel = 0; channel < channels; ++channel)
+                {
+                    for (uint32_t sample = 0; sample < frames; ++sample)
+                        channelSamples[sample] = samples[sample * channels + channel];
+
+                    pitchShift[channel].process(pitch, frames, 1024, 4, static_cast<float>(sampleRate), channelSamples.data(), channelSamples.data());
+
+                    for (uint32_t sample = 0; sample < frames; ++sample)
+                        samples[sample * channels + channel] = channelSamples[sample];
+                }
+            }
+
+            void setPitch(float newPitch)
+            {
+                pitch = clamp(newPitch, MIN_PITCH, MAX_PITCH);
+            }
+
+        private:
+            float pitch = 1.0f;
+            std::vector<float> channelSamples;
+            std::vector<smb::PitchShift> pitchShift;
+        };
+
+        Pitch::Pitch(Audio& initAudio, float initPitch):
+            Filter(initAudio,
+                   initAudio.initProcessor(std::unique_ptr<Processor>(new PitchProcessor(initPitch)))),
+            pitch(initPitch)
+        {
+        }
+
+        Pitch::~Pitch()
+        {
+        }
+
+        void Pitch::setPitch(float newPitch)
+        {
+            pitch = newPitch;
+
+            audio.updateProcessor(processorId, [newPitch](Object* node) {
+                PitchProcessor* pitchProcessor = static_cast<PitchProcessor*>(node);
+                pitchProcessor->setPitch(newPitch);
+            });
+        }
+
+        class ReverbProcessor final: public Processor
+        {
+        public:
+            ReverbProcessor()
+            {
+            }
+
+            void process(uint32_t frames, uint16_t channels, uint32_t sampleRate,
+                         std::vector<float>& samples) override
+            {
+            }
+        };
+
+        Reverb::Reverb(Audio& initAudio):
+            Filter(initAudio,
+            initAudio.initProcessor(std::unique_ptr<Processor>(new ReverbProcessor())))
+        {
+        }
+
+        Reverb::~Reverb()
+        {
+        }
+    } // namespace audio
+} // namespace ouzel
