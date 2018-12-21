f789e074b7c67afa2db9255acabb995728a868a7
Elviss Strazdins <elviss@elviss.lv>
2018-12-21 15:47:52 +0200

    Remove the unused include

--- ouzel/utils/Log.cpp
+++ ouzel/utils/Log.cpp
@@ -1,7 +1,6 @@
 // Copyright 2015-2018 Elviss Strazdins. All rights reserved.
 
 #include "core/Setup.h"
-#include <string>
 
 #if OUZEL_PLATFORM_MACOS || OUZEL_PLATFORM_LINUX
 #  include <unistd.h>
