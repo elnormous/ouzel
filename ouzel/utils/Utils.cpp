e94acee021f2aebd72e6285c55f2a289bc4ff447
Elviss Strazdins <elviss@elviss.lv>
2019-01-03 00:17:32 +0200

    Move SIMD detection to MathUtils.hpp

--- ouzel/utils/Utils.cpp
+++ ouzel/utils/Utils.cpp
@@ -23,11 +23,6 @@ typedef struct tagTHREADNAME_INFO
 
 namespace ouzel
 {
-#if defined(__ANDROID__) && defined(__ARM_NEON__) && defined(__arm__)
-    // NEON support must be checked at runtime on 32-bit Android
-    AnrdoidNeonChecker isSimdAvailable;
-#endif
-
     std::mt19937 randomEngine(std::random_device{}());
 
     void setCurrentThreadName(const std::string& name)
