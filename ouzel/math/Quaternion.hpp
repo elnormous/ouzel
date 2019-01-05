a86e5e776b6f76af203fe1a7b1f9c716a606c972
Elviss Strazdins <elviss@elviss.lv>
2019-01-06 00:41:52 +0200

    Get rid of the Quaternion.cpp Remove unused Utils.hpp include from Color.cpp

--- ouzel/math/Quaternion.hpp
+++ ouzel/math/Quaternion.hpp
@@ -192,10 +192,62 @@ namespace ouzel
             v[3] = v[3] / n2;
         }
 
-        float getNorm();
-        void normalize();
-        void rotate(float angle, Vector3<T> axis);
-        void getRotation(float& angle, Vector3<T>& axis);
+        float getNorm() const
+        {
+            float n = v[0] * v[0] + v[1] * v[1] + v[2] * v[2] + v[3] * v[3];
+            if (n == 1.0F) // already normalized
+                return 1.0F;
+
+            return sqrtf(n);
+        }
+
+        void normalize()
+        {
+            float n = v[0] * v[0] + v[1] * v[1] + v[2] * v[2] + v[3] * v[3];
+            if (n == 1.0F) // already normalized
+                return;
+
+            n = sqrtf(n);
+            if (n < std::numeric_limits<float>::min()) // too close to zero
+                return;
+
+            n = 1.0F / n;
+            v[0] *= n;
+            v[1] *= n;
+            v[2] *= n;
+            v[3] *= n;
+        }
+
+        void rotate(float angle, Vector3<T> axis)
+        {
+            axis.normalize();
+
+            float cosAngle = cosf(angle / 2.0F);
+            float sinAngle = sinf(angle / 2.0F);
+
+            v[0] = axis.v[0] * sinAngle;
+            v[1] = axis.v[1] * sinAngle;
+            v[2] = axis.v[2] * sinAngle;
+            v[3] = cosAngle;
+        }
+
+        void getRotation(float& angle, Vector3<T>& axis)
+        {
+            angle = 2.0F * acosf(v[3]);
+            float s = sqrtf(1.0F - v[3] * v[3]);
+            if (s < std::numeric_limits<float>::min()) // too close to zero
+            {
+                axis.v[0] = v[0];
+                axis.v[1] = v[1];
+                axis.v[2] = v[2];
+            }
+            else
+            {
+                axis.v[0] = v[0] / s;
+                axis.v[1] = v[1] / s;
+                axis.v[2] = v[2] / s;
+            }
+        }
 
         Vector3<T> getEulerAngles() const
         {
@@ -221,7 +273,32 @@ namespace ouzel
             return atan2f(2.0F * (v[0] * v[1] + v[3] * v[2]), v[3] * v[3] + v[0] * v[0] - v[1] * v[1] - v[2] * v[2]);
         }
         
-        void setEulerAngles(const Vector3<T>& angles);
+        void setEulerAngles(const Vector3<T>& angles)
+        {
+            float angle;
+
+            angle = angles.v[0] * 0.5F;
+            const float sr = sinf(angle);
+            const float cr = cosf(angle);
+
+            angle = angles.v[1] * 0.5F;
+            const float sp = sinf(angle);
+            const float cp = cosf(angle);
+
+            angle = angles.v[2] * 0.5F;
+            const float sy = sinf(angle);
+            const float cy = cosf(angle);
+
+            const float cpcy = cp * cy;
+            const float spcy = sp * cy;
+            const float cpsy = cp * sy;
+            const float spsy = sp * sy;
+
+            v[0] = sr * cpcy - cr * spsy;
+            v[1] = cr * spcy + sr * cpsy;
+            v[2] = cr * cpsy - sr * spcy;
+            v[3] = cr * cpcy + sr * spsy;
+        }
 
         inline const Vector3<T> operator*(const Vector3<T>& vector) const
         {
