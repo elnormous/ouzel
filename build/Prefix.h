22ea86d8267d661e520bbf4897c4b27bd189f137
Elviss Strazdins <elviss@elviss.lv>
2019-01-09 04:51:49 +0200

    Add Prefix.h for Android

---
+++ build/Prefix.h
@@ -0,0 +1,43 @@
+// Copyright 2015-2018 Elviss Strazdins. All rights reserved.
+
+#ifndef PREFIX_H
+#define PREFIX_H
+
+#ifdef __cplusplus
+#  include <algorithm>
+#  include <array>
+#  include <atomic>
+#  include <cassert>
+#  include <cctype>
+#  include <cfloat>
+#  include <chrono>
+#  include <cmath>
+#  include <condition_variable>
+#  include <cstddef>
+#  include <cstdint>
+#  include <cstdlib>
+#  include <functional>
+#  include <future>
+#  include <iterator>
+#  include <limits>
+#  include <list>
+#  include <map>
+#  include <memory>
+#  include <mutex>
+#  include <numeric>
+#  include <queue>
+#  include <random>
+#  include <set>
+#  include <sstream>
+#  include <stdexcept>
+#  include <string>
+#  include <system_error>
+#  include <thread>
+#  include <tuple>
+#  include <type_traits>
+#  include <unordered_map>
+#  include <utility>
+#  include <vector>
+#endif
+
+#endif // PREFIX_H
