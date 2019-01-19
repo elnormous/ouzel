0d2a54387737913bc02b03e8f52ae6569d4a024f
Elviss Strazdins <elviss@elviss.lv>
2019-01-19 03:19:02 +0200

    Rename D to DEPTH and DS to DEPTH_STENCIL Fix D3D11 depth stencil vies initialization

--- ouzel/graphics/direct3d11/D3D11Texture.cpp
+++ ouzel/graphics/direct3d11/D3D11Texture.cpp
@@ -45,8 +45,8 @@ namespace ouzel
                 case PixelFormat::RGBA32_UINT: return DXGI_FORMAT_R32G32B32A32_UINT;
                 case PixelFormat::RGBA32_SINT: return DXGI_FORMAT_R32G32B32A32_SINT;
                 case PixelFormat::RGBA32_FLOAT: return DXGI_FORMAT_R32G32B32A32_FLOAT;
-                case PixelFormat::D: return DXGI_FORMAT_D32_FLOAT;
-                case PixelFormat::DS: return DXGI_FORMAT_D24_UNORM_S8_UINT;
+                case PixelFormat::DEPTH: return DXGI_FORMAT_D32_FLOAT;
+                case PixelFormat::DEPTH_STENCIL: return DXGI_FORMAT_D24_UNORM_S8_UINT;
                 default: return DXGI_FORMAT_UNKNOWN;
             }
         }
