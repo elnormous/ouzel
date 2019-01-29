ff063005b1823c0d5b076c414e71acf24ac8004f
Elviss Strazdins <elviss@elviss.lv>
2019-01-29 11:21:59 +0200

    Initialize depth stencil state for D3D11

--- ouzel/graphics/direct3d11/D3D11DepthStencilState.cpp
+++ ouzel/graphics/direct3d11/D3D11DepthStencilState.cpp
@@ -28,29 +28,47 @@ namespace ouzel
             return D3D11_COMPARISON_NEVER;
         }
 
+        static D3D11_STENCIL_OP getStencilOperation(DepthStencilState::StencilOperation stencilOperation)
+        {
+            switch (stencilOperation)
+            {
+                case DepthStencilState::StencilOperation::KEEP: return D3D11_STENCIL_OP_KEEP;
+                case DepthStencilState::StencilOperation::ZERO: return D3D11_STENCIL_OP_ZERO;
+                case DepthStencilState::StencilOperation::REPLACE: return D3D11_STENCIL_OP_REPLACE;
+                case DepthStencilState::StencilOperation::INCREMENT_CLAMP: return D3D11_STENCIL_OP_INCR_SAT;
+                case DepthStencilState::StencilOperation::DECREMENT_CLAMP: return D3D11_STENCIL_OP_DECR_SAT;
+                case DepthStencilState::StencilOperation::INVERT: return D3D11_STENCIL_OP_INVERT;
+                case DepthStencilState::StencilOperation::INCREMENT_WRAP: return D3D11_STENCIL_OP_INCR;
+                case DepthStencilState::StencilOperation::DECREMENT_WRAP: return D3D11_STENCIL_OP_DECR;
+            }
+        }
+
         D3D11DepthStencilState::D3D11DepthStencilState(D3D11RenderDevice& renderDeviceD3D11,
                                                        bool initDepthTest,
                                                        bool initDepthWrite,
                                                        DepthStencilState::CompareFunction initCompareFunction,
+                                                       bool initStencilEnabled,
                                                        uint32_t initStencilReadMask,
-                                                       uint32_t initStencilWriteMask):
+                                                       uint32_t initStencilWriteMask,
+                                                       const DepthStencilState::StencilDescriptor& initFrontFaceStencil,
+                                                       const DepthStencilState::StencilDescriptor& initBackFaceStencil):
             D3D11RenderResource(renderDeviceD3D11)
         {
             D3D11_DEPTH_STENCIL_DESC depthStencilStateDesc;
             depthStencilStateDesc.DepthEnable = initDepthTest ? TRUE : FALSE;
             depthStencilStateDesc.DepthWriteMask = initDepthWrite ? D3D11_DEPTH_WRITE_MASK_ALL : D3D11_DEPTH_WRITE_MASK_ZERO;
             depthStencilStateDesc.DepthFunc = getCompareFunction(initCompareFunction);
-            depthStencilStateDesc.StencilEnable = FALSE;
+            depthStencilStateDesc.StencilEnable = initStencilEnabled ? TRUE : FALSE;
             depthStencilStateDesc.StencilReadMask = static_cast<UINT8>(initStencilReadMask);
             depthStencilStateDesc.StencilWriteMask = static_cast<UINT8>(initStencilWriteMask);
-            depthStencilStateDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-            depthStencilStateDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-            depthStencilStateDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
-            depthStencilStateDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
-            depthStencilStateDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-            depthStencilStateDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
-            depthStencilStateDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
-            depthStencilStateDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+            depthStencilStateDesc.FrontFace.StencilFailOp = getStencilOperation(initFrontFaceStencil.failureOperation);
+            depthStencilStateDesc.FrontFace.StencilDepthFailOp = getStencilOperation(initFrontFaceStencil.depthFailureOperation);
+            depthStencilStateDesc.FrontFace.StencilPassOp = getStencilOperation(initFrontFaceStencil.passOperation);
+            depthStencilStateDesc.FrontFace.StencilFunc = getCompareFunction(initFrontFaceStencil.compareFunction);
+            depthStencilStateDesc.BackFace.StencilFailOp = getStencilOperation(initBackFaceStencil.failureOperation);
+            depthStencilStateDesc.BackFace.StencilDepthFailOp = getStencilOperation(initBackFaceStencil.depthFailureOperation);
+            depthStencilStateDesc.BackFace.StencilPassOp = getStencilOperation(initBackFaceStencil.passOperation);
+            depthStencilStateDesc.BackFace.StencilFunc = getCompareFunction(initBackFaceStencil.compareFunction);
 
             HRESULT hr;
             if (FAILED(hr = renderDevice.getDevice()->CreateDepthStencilState(&depthStencilStateDesc, &depthStencilState)))
