b8087ca3e1c94071c6bfbc29caacf85d17e59feb
Elviss Strazdins <elviss@elviss.lv>
2019-02-18 01:37:53 +0200

    Use switch instead of a map for key code lookup on macOS, iOS, and tvOS

--- ouzel/input/emscripten/MouseDeviceEm.cpp
+++ ouzel/input/emscripten/MouseDeviceEm.cpp
@@ -17,14 +17,14 @@ namespace ouzel
             {
                 // TODO: replace this with emscripten_show_mouse when https://github.com/kripken/emscripten/issues/4614 is merged
                 emscripten_run_script("var styleSheet = document.styleSheets[0];"
-                                        "var rules = styleSheet.cssRules;"
-                                        "for (var i = 0; i < rules.length; i++) {"
-                                        "if (rules[i].cssText.substr(0, 6) == 'canvas') {"
-                                        "styleSheet.deleteRule(i);"
-                                        "i--;"
-                                        "}"
-                                        "}"
-                                        "styleSheet.insertRule('canvas.emscripten { border: none; cursor: auto; }', 0);");
+                                      "var rules = styleSheet.cssRules;"
+                                      "for (var i = 0; i < rules.length; i++) {"
+                                      "if (rules[i].cssText.substr(0, 6) == 'canvas') {"
+                                      "styleSheet.deleteRule(i);"
+                                      "i--;"
+                                      "}"
+                                      "}"
+                                      "styleSheet.insertRule('canvas.emscripten { border: none; cursor: auto; }', 0);");
             }
         }
 
