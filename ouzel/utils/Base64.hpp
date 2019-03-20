632f319a2eb0e346aa8cf8ccd83925640bb8a98b
Elviss Strazdins <elviss@elviss.lv>
2019-03-20 01:24:30 -0400

    Fix warnings in Base64.hpp

--- ouzel/utils/Base64.hpp
+++ ouzel/utils/Base64.hpp
@@ -1,7 +1,7 @@
 // Copyright 2015-2019 Elviss Strazdins. All rights reserved.
 
-#ifndef OUZEL_BASE64_HPP
-#define OUZEL_BASE64_HPP
+#ifndef OUZE_UTILS_BASE64_HPP
+#define OUZE_UTILS_BASE64_HPP
 
 #include <cstdint>
 #include <string>
@@ -11,15 +11,28 @@ namespace ouzel
 {
     namespace base64
     {
-        static const std::string CHARS =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz"
-            "0123456789+/";
+        static const char CHARS[] = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+        };
 
-
-        static inline bool isBase64(unsigned char c)
+        static inline bool isBase64(char c)
         {
-            return (isalnum(c) || (c == '+') || (c == '/'));
+            return ((c >= 'A' && c <= 'Z') ||
+                    (c >= 'a' && c <= 'z') ||
+                    (c >= '0' && c <= '9') ||
+                    (c == '+') || (c == '/'));
+        }
+
+        static inline uint8_t charIndex(uint8_t c)
+        {
+            for (uint8_t i = 0; i < sizeof(CHARS); ++i)
+                if (CHARS[i] == c) return i;
+
+            throw std::runtime_error("Invalid character");
         }
 
         inline std::string encode(const std::vector<uint8_t>& bytes)
@@ -31,17 +44,17 @@ namespace ouzel
             uint8_t charArray4[4];
             const uint8_t* bytesToEncode = bytes.data();
 
-            for (int l = 0; l < bytes.size(); ++l)
+            for (uint32_t l = 0; l < bytes.size(); ++l)
             {
                 charArray3[i++] = *(bytesToEncode++);
                 if (i == 3)
                 {
-                    charArray4[0] = (charArray3[0] & 0xFC) >> 2;
-                    charArray4[1] = ((charArray3[0] & 0x03) << 4) + ((charArray3[1] & 0xF0) >> 4);
-                    charArray4[2] = ((charArray3[1] & 0x0F) << 2) + ((charArray3[2] & 0xC0) >> 6);
-                    charArray4[3] = charArray3[2] & 0x3f;
+                    charArray4[0] = static_cast<uint8_t>((charArray3[0] & 0xFC) >> 2);
+                    charArray4[1] = static_cast<uint8_t>(((charArray3[0] & 0x03) << 4) + ((charArray3[1] & 0xF0) >> 4));
+                    charArray4[2] = static_cast<uint8_t>(((charArray3[1] & 0x0F) << 2) + ((charArray3[2] & 0xC0) >> 6));
+                    charArray4[3] = static_cast<uint8_t>(charArray3[2] & 0x3f);
 
-                    for(i = 0; (i <4) ; i++) result += CHARS[charArray4[i]];
+                    for (i = 0; (i < 4) ; i++) result += CHARS[charArray4[i]];
                     i = 0;
                 }
             }
@@ -50,9 +63,9 @@ namespace ouzel
             {
                 for (j = i; j < 3; j++) charArray3[j] = '\0';
 
-                charArray4[0] = (charArray3[0] & 0xFC) >> 2;
-                charArray4[1] = ((charArray3[0] & 0x03) << 4) + ((charArray3[1] & 0xF0) >> 4);
-                charArray4[2] = ((charArray3[1] & 0x0F) << 2) + ((charArray3[2] & 0xC0) >> 6);
+                charArray4[0] = static_cast<uint8_t>((charArray3[0] & 0xFC) >> 2);
+                charArray4[1] = static_cast<uint8_t>(((charArray3[0] & 0x03) << 4) + ((charArray3[1] & 0xF0) >> 4));
+                charArray4[2] = static_cast<uint8_t>(((charArray3[1] & 0x0F) << 2) + ((charArray3[2] & 0xC0) >> 6));
 
                 for (j = 0; (j < i + 1); j++) result += CHARS[charArray4[j]];
 
@@ -65,23 +78,22 @@ namespace ouzel
 
         inline std::vector<uint8_t> decode(const std::string& encodedString)
         {
-            int i = 0;
-            int j = 0;
-            int in = 0;
+            uint32_t i = 0;
+            uint32_t in = 0;
             uint8_t charArray3[3];
             uint8_t charArray4[4];
             std::vector<uint8_t> result;
 
-            for (int l = 0; l < encodedString.size() && encodedString[in] != '=' && isBase64(encodedString[in]); ++l)
+            for (uint32_t l = 0; l < encodedString.size() && encodedString[in] != '='; ++l)
             {
-                charArray4[i++] = encodedString[in]; in++;
+                charArray4[i++] = static_cast<uint8_t>(encodedString[in]); in++;
                 if (i == 4)
                 {
-                    for (i = 0; i <4; i++) charArray4[i] = CHARS.find(charArray4[i]);
+                    for (i = 0; i < 4; i++) charArray4[i] = charIndex(charArray4[i]);
 
-                    charArray3[0] = (charArray4[0] << 2) + ((charArray4[1] & 0x30) >> 4);
-                    charArray3[1] = ((charArray4[1] & 0x0F) << 4) + ((charArray4[2] & 0x3C) >> 2);
-                    charArray3[2] = ((charArray4[2] & 0x3) << 6) + charArray4[3];
+                    charArray3[0] = static_cast<uint8_t>((charArray4[0] << 2) + ((charArray4[1] & 0x30) >> 4));
+                    charArray3[1] = static_cast<uint8_t>(((charArray4[1] & 0x0F) << 4) + ((charArray4[2] & 0x3C) >> 2));
+                    charArray3[2] = static_cast<uint8_t>(((charArray4[2] & 0x3) << 6) + charArray4[3]);
 
                     for (i = 0; (i < 3); i++) result.push_back(charArray3[i]);
                     i = 0;
@@ -90,12 +102,12 @@ namespace ouzel
 
             if (i)
             {
-                for (j = 0; j < i; j++) charArray4[j] = CHARS.find(charArray4[j]);
+                for (uint32_t j = 0; j < i; j++) charArray4[j] = charIndex(charArray4[j]);
 
-                charArray3[0] = (charArray4[0] << 2) + ((charArray4[1] & 0x30) >> 4);
-                charArray3[1] = ((charArray4[1] & 0x0F) << 4) + ((charArray4[2] & 0x3C) >> 2);
+                charArray3[0] = static_cast<uint8_t>((charArray4[0] << 2) + ((charArray4[1] & 0x30) >> 4));
+                charArray3[1] = static_cast<uint8_t>(((charArray4[1] & 0x0F) << 4) + ((charArray4[2] & 0x3C) >> 2));
 
-                for (j = 0; (j < i - 1); j++) result.push_back(charArray3[j]);
+                for (uint32_t j = 0; (j < i - 1); j++) result.push_back(charArray3[j]);
             }
 
             return result;
@@ -103,4 +115,4 @@ namespace ouzel
     } // namespace base64
 } // namespace ouzel
 
-#endif // OUZEL_BASE64_HPP
+#endif // OUZE_UTILS_BASE64_HPP
