31a80009bad958d11f01fda22f49e38afa2fdfa7
Elviss Strazdins <elviss@elviss.lv>
2019-06-01 02:19:40 +0300

    Change casing of hex literals

--- ouzel/utils/Utf8.hpp
+++ ouzel/utils/Utf8.hpp
@@ -18,32 +18,32 @@ namespace ouzel
 
             for (auto i = text.begin(); i != text.end(); ++i)
             {
-                uint32_t cp = *i & 0xff;
+                uint32_t cp = *i & 0xFF;
 
-                if (cp <= 0x7f) // length = 1
+                if (cp <= 0x7F) // length = 1
                 {
                     // do nothing
                 }
                 else if ((cp >> 5) == 0x6) // length = 2
                 {
                     if (++i == text.end()) return result;
-                    cp = ((cp << 6) & 0x7ff) + (*i & 0x3f);
+                    cp = ((cp << 6) & 0x7FF) + (*i & 0x3F);
                 }
-                else if ((cp >> 4) == 0xe) // length = 3
+                else if ((cp >> 4) == 0xE) // length = 3
                 {
                     if (++i == text.end()) return result;
-                    cp = ((cp << 12) & 0xffff) + (((*i & 0xff) << 6) & 0xfff);
+                    cp = ((cp << 12) & 0xFFFF) + (((*i & 0xFF) << 6) & 0x0FFF);
                     if (++i == text.end()) return result;
-                    cp += *i & 0x3f;
+                    cp += *i & 0x3F;
                 }
-                else if ((cp >> 3) == 0x1e) // length = 4
+                else if ((cp >> 3) == 0x1E) // length = 4
                 {
                     if (++i == text.end()) return result;
-                    cp = ((cp << 18) & 0x1fffff) + (((*i & 0xff) << 12) & 0x3ffff);
+                    cp = ((cp << 18) & 0x1FFFFF) + (((*i & 0xFF) << 12) & 0x3FFFF);
                     if (++i == text.end()) return result;
-                    cp += ((*i & 0xff) << 6) & 0xfff;
+                    cp += ((*i & 0xFF) << 6) & 0x0FFF;
                     if (++i == text.end()) return result;
-                    cp += (*i) & 0x3f;
+                    cp += (*i) & 0x3F;
                 }
 
                 result.push_back(cp);
@@ -56,25 +56,25 @@ namespace ouzel
         {
             std::string result;
 
-            if (c <= 0x7f)
+            if (c <= 0x7F)
                 result.push_back(static_cast<char>(c));
-            else if (c <= 0x7ff)
+            else if (c <= 0x7FF)
             {
-                result.push_back(static_cast<char>(0xc0 | ((c >> 6) & 0x1f)));
-                result.push_back(static_cast<char>(0x80 | (c & 0x3f)));
+                result.push_back(static_cast<char>(0xC0 | ((c >> 6) & 0x1F)));
+                result.push_back(static_cast<char>(0x80 | (c & 0x3F)));
             }
-            else if (c <= 0xffff)
+            else if (c <= 0xFFFF)
             {
-                result.push_back(static_cast<char>(0xe0 | ((c >> 12) & 0x0f)));
-                result.push_back(static_cast<char>(0x80 | ((c >> 6) & 0x3f)));
-                result.push_back(static_cast<char>(0x80 | (c & 0x3f)));
+                result.push_back(static_cast<char>(0xE0 | ((c >> 12) & 0x0F)));
+                result.push_back(static_cast<char>(0x80 | ((c >> 6) & 0x3F)));
+                result.push_back(static_cast<char>(0x80 | (c & 0x3F)));
             }
             else
             {
-                result.push_back(static_cast<char>(0xf0 | ((c >> 18) & 0x07)));
-                result.push_back(static_cast<char>(0x80 | ((c >> 12) & 0x3f)));
-                result.push_back(static_cast<char>(0x80 | ((c >> 6) & 0x3f)));
-                result.push_back(static_cast<char>(0x80 | (c & 0x3f)));
+                result.push_back(static_cast<char>(0xF0 | ((c >> 18) & 0x07)));
+                result.push_back(static_cast<char>(0x80 | ((c >> 12) & 0x3F)));
+                result.push_back(static_cast<char>(0x80 | ((c >> 6) & 0x3F)));
+                result.push_back(static_cast<char>(0x80 | (c & 0x3F)));
             }
 
             return result;
@@ -86,25 +86,25 @@ namespace ouzel
 
             for (auto i = text.begin(); i != text.end(); ++i)
             {
-                if (*i <= 0x7f)
+                if (*i <= 0x7F)
                     result.push_back(static_cast<char>(*i));
-                else if (*i <= 0x7ff)
+                else if (*i <= 0x7FF)
                 {
-                    result.push_back(static_cast<char>(0xc0 | ((*i >> 6) & 0x1f)));
-                    result.push_back(static_cast<char>(0x80 | (*i & 0x3f)));
+                    result.push_back(static_cast<char>(0xC0 | ((*i >> 6) & 0x1F)));
+                    result.push_back(static_cast<char>(0x80 | (*i & 0x3F)));
                 }
-                else if (*i <= 0xffff)
+                else if (*i <= 0xFFFF)
                 {
-                    result.push_back(static_cast<char>(0xe0 | ((*i >> 12) & 0x0f)));
-                    result.push_back(static_cast<char>(0x80 | ((*i >> 6) & 0x3f)));
-                    result.push_back(static_cast<char>(0x80 | (*i & 0x3f)));
+                    result.push_back(static_cast<char>(0xE0 | ((*i >> 12) & 0x0F)));
+                    result.push_back(static_cast<char>(0x80 | ((*i >> 6) & 0x3F)));
+                    result.push_back(static_cast<char>(0x80 | (*i & 0x3F)));
                 }
                 else
                 {
-                    result.push_back(static_cast<char>(0xf0 | ((*i >> 18) & 0x07)));
-                    result.push_back(static_cast<char>(0x80 | ((*i >> 12) & 0x3f)));
-                    result.push_back(static_cast<char>(0x80 | ((*i >> 6) & 0x3f)));
-                    result.push_back(static_cast<char>(0x80 | (*i & 0x3f)));
+                    result.push_back(static_cast<char>(0xF0 | ((*i >> 18) & 0x07)));
+                    result.push_back(static_cast<char>(0x80 | ((*i >> 12) & 0x3F)));
+                    result.push_back(static_cast<char>(0x80 | ((*i >> 6) & 0x3F)));
+                    result.push_back(static_cast<char>(0x80 | (*i & 0x3F)));
                 }
             }
 
