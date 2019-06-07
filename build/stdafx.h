8ba1fca2f57ab5b2aee9b681a29b2faaf48e270e
Elviss Strazdins <elviss@elviss.lv>
2019-06-07 22:08:14 +0300

    Add includes for Android and Emscripten to the precompiled header Correctly restore WIN32_LEAN_AND_MEAN and NOMINMAX macros in stdafx.h

--- build/stdafx.h
+++ build/stdafx.h
@@ -3,11 +3,17 @@
 #ifndef STDAFX_H
 #define STDAFX_H
 
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
+#pragma push_macro("WIN32_LEAN_AND_MEAN")
+#pragma push_macro("NOMINMAX")
+#ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#  define NOMINMAX
+#endif
 #include <Windows.h>
-#undef WIN32_LEAN_AND_MEAN
-#undef NOMINMAX
+#pragma pop_macro("WIN32_LEAN_AND_MEAN")
+#pragma pop_macro("NOMINMAX")
 
 #ifdef __cplusplus
 #  include <algorithm>
