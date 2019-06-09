efb7947886e0ed7f103d9fc6d725a86bd05092f3
Elviss Strazdins <elviss@elviss.lv>
2019-06-09 22:12:26 +0300

    Fix the include guards for PcmClip.hpp and VorbisClip.hpp

--- ouzel/audio/PcmClip.hpp
+++ ouzel/audio/PcmClip.hpp
@@ -1,7 +1,7 @@
 // Copyright 2015-2019 Elviss Strazdins. All rights reserved.
 
-#ifndef OUZEL_AUDIO_PCMSOUND_HPP
-#define OUZEL_AUDIO_PCMSOUND_HPP
+#ifndef OUZEL_AUDIO_PCMCLIP_HPP
+#define OUZEL_AUDIO_PCMCLIP_HPP
 
 #include <cstdint>
 #include <vector>
@@ -25,4 +25,4 @@ namespace ouzel
     } // namespace audio
 } // namespace ouzel
 
-#endif // OUZEL_AUDIO_PCMSOUND_HPP
+#endif // OUZEL_AUDIO_PCMCLIP_HPP
--- ouzel/audio/VorbisClip.hpp
+++ ouzel/audio/VorbisClip.hpp
@@ -1,7 +1,7 @@
 // Copyright 2015-2019 Elviss Strazdins. All rights reserved.
 
-#ifndef OUZEL_AUDIO_VORBISSOUND_HPP
-#define OUZEL_AUDIO_VORBISSOUND_HPP
+#ifndef OUZEL_AUDIO_VORBISCLIP_HPP
+#define OUZEL_AUDIO_VORBISCLIP_HPP
 
 #include <cstdint>
 #include <vector>
@@ -24,4 +24,4 @@ namespace ouzel
     } // namespace audio
 } // namespace ouzel
 
-#endif // OUZEL_AUDIO_VORBISSOUND_HPP
+#endif // OUZEL_AUDIO_VORBISCLIP_HPP
