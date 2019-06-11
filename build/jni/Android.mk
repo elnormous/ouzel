a5048ac5caf7c90618e09ff7b997c55c0aac658f
Elviss Strazdins <elviss@elviss.lv>
2019-06-12 00:00:33 +0300

    Remove Font.cpp

--- build/jni/Android.mk
+++ build/jni/Android.mk
@@ -80,7 +80,6 @@ LOCAL_SRC_FILES := ../../ouzel/assets/BmfLoader.cpp \
     ../../ouzel/graphics/Texture.cpp \
     ../../ouzel/gui/BMFont.cpp \
     ../../ouzel/gui/TTFont.cpp \
-    ../../ouzel/gui/Font.cpp \
     ../../ouzel/gui/Widget.cpp \
 	../../ouzel/gui/Widgets.cpp \
     ../../ouzel/input/android/GamepadDeviceAndroid.cpp \
