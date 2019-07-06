2bd0413a0bbd60f7ff1cd1343c686653049ddf5b
Elviss Strazdins <elviss@elviss.lv>
2019-07-06 03:08:36 +0300

    Move cue definitions to submixes

--- ouzel/audio/Bank.hpp
+++ ouzel/audio/Bank.hpp
@@ -5,11 +5,14 @@
 
 #include <string>
 #include <vector>
+#include "Voice.hpp"
 
 namespace ouzel
 {
     namespace audio
     {
+        class Audio;
+
         struct AttributeDefinition final
         {
             std::string name;
@@ -18,13 +21,6 @@ namespace ouzel
 
         struct FilterDefinition final
         {
-            std::vector<AttributeDefinition> attributeDefinitions;
-        };
-
-        struct BusDefinition final
-        {
-            std::vector<BusDefinition> inputDefinitions;
-            std::vector<FilterDefinition> filterDefinitions;
             std::vector<AttributeDefinition> attributeDefinitions;
         };
 
@@ -37,25 +33,70 @@ namespace ouzel
 
         struct CueDefinition final
         {
-            // TODO: bus ID
             std::vector<SourceDefinition> sourceDefinitions;
+        };
+
+        struct SubmixDefinition final
+        {
+            std::vector<SubmixDefinition> inputDefinitions;
+            std::vector<FilterDefinition> filterDefinitions;
+            std::vector<AttributeDefinition> attributeDefinitions;
+            std::vector<CueDefinition> cueDefinitions;
         };
 
         struct BankDefinition final
         {
-            std::vector<BusDefinition> busDefinitions;
-            std::vector<CueDefinition> cueDefinitions;
+            std::vector<SubmixDefinition> submixDefinitions;
         };
 
         class Bank
         {
         public:
-            Bank(const BankDefinition& bankDefinition):
-                definition(bankDefinition)
+            Bank(Audio& initAudio):
+                audio(initAudio)
             {
             }
 
+            Bank(Audio& initAudio,
+                 const BankDefinition& bankDefinition):
+                audio(initAudio),
+                definition(bankDefinition)
+            {
+                // TODO: create command buffer
+
+                for (const SubmixDefinition& submixDefinition : bankDefinition.submixDefinitions)
+                {
+                    for (const SubmixDefinition& inputDefinition : submixDefinition.inputDefinitions)
+                    {
+
+                    }
+
+                    for (const FilterDefinition& filterDefinition : submixDefinition.filterDefinitions)
+                    {
+
+                    }
+
+                    for (const AttributeDefinition& attributeDefinition : submixDefinition.attributeDefinitions)
+                    {
+
+                    }
+
+                    for (const CueDefinition& cueDefinition : submixDefinition.cueDefinitions)
+                    {
+
+                    }
+                    // TODO: add create bus commands to buffer
+                }
+            }
+
+            Voice createVoice()
+            {
+                // TODO: create cue
+                return Voice(audio);
+            }
+
         private:
+            Audio& audio;
             BankDefinition definition;
         };
     } // namespace audio
