e1b28b9e6029bfa979be83566805de638c1eb600
Elviss Strazdins <elviss@elviss.lv>
2019-08-14 01:07:52 +0300

    Disable the signed/unsigned mismatch warning for stb_vorbis.c

--- ouzel/audio/VorbisClip.cpp
+++ ouzel/audio/VorbisClip.cpp
@@ -11,6 +11,7 @@
 #  pragma warning( push )
 #  pragma warning( disable : 4100 )
 #  pragma warning( disable : 4244 )
+#  pragma warning( disable : 4245 )
 #  pragma warning( disable : 4456 )
 #  pragma warning( disable : 4457 )
 #elif defined(__GNUC__)
