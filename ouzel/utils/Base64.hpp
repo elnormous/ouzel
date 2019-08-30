385e52609653b4e4b9866075f3a15fa3ceb2d468
Elviss Strazdins <elviss@elviss.lv>
2019-08-30 23:58:19 +0300

    Make the CHARS array constexpr

--- ouzel/utils/Base64.hpp
+++ ouzel/utils/Base64.hpp
@@ -12,7 +12,7 @@ namespace ouzel
 {
     namespace base64
     {
-        static const char CHARS[] = {
+        static constexpr char CHARS[] = {
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
