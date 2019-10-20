3cb45cd6037e110ca8af6fe95a65ce8e9c4703f5
Elviss Strazdins <elviss@elviss.lv>
2019-10-20 19:06:06 +0300

    Add implementation for Matrix::subtract

--- ouzel/math/Matrix.hpp
+++ ouzel/math/Matrix.hpp
@@ -673,7 +673,25 @@ namespace ouzel
             subtract(*this, matrix, *this);
         }
 
-        static void subtract(const Matrix& m1, const Matrix& m2, Matrix& dst);
+        static void subtract(const Matrix& m1, const Matrix& m2, Matrix& dst)
+        {
+            dst.m[0] = m1.m[0] - m2.m[0];
+            dst.m[1] = m1.m[1] - m2.m[1];
+            dst.m[2] = m1.m[2] - m2.m[2];
+            dst.m[3] = m1.m[3] - m2.m[3];
+            dst.m[4] = m1.m[4] - m2.m[4];
+            dst.m[5] = m1.m[5] - m2.m[5];
+            dst.m[6] = m1.m[6] - m2.m[6];
+            dst.m[7] = m1.m[7] - m2.m[7];
+            dst.m[8] = m1.m[8] - m2.m[8];
+            dst.m[9] = m1.m[9] - m2.m[9];
+            dst.m[10] = m1.m[10] - m2.m[10];
+            dst.m[11] = m1.m[11] - m2.m[11];
+            dst.m[12] = m1.m[12] - m2.m[12];
+            dst.m[13] = m1.m[13] - m2.m[13];
+            dst.m[14] = m1.m[14] - m2.m[14];
+            dst.m[15] = m1.m[15] - m2.m[15];
+        }
 
         template <size_t X = C, size_t Y = R, typename std::enable_if<(X == 4 && Y == 4)>::type* = nullptr>
         void transformPoint(Vector<3, T>& point) const noexcept
