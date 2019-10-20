483af8277dea10ca43b9014ff4c5aa059ecc9cc0
Elviss Strazdins <elviss@elviss.lv>
2019-10-20 19:14:38 +0300

    Remove the unneeded definition of Matrix::subtract

--- ouzel/math/Matrix.cpp
+++ ouzel/math/Matrix.cpp
@@ -530,27 +530,6 @@ namespace ouzel
         }
     }
 
-    template <size_t C, size_t R, typename T>
-    void Matrix<C, R, T>::subtract(const Matrix& m1, const Matrix& m2, Matrix& dst)
-    {
-        dst.m[0] = m1.m[0] - m2.m[0];
-        dst.m[1] = m1.m[1] - m2.m[1];
-        dst.m[2] = m1.m[2] - m2.m[2];
-        dst.m[3] = m1.m[3] - m2.m[3];
-        dst.m[4] = m1.m[4] - m2.m[4];
-        dst.m[5] = m1.m[5] - m2.m[5];
-        dst.m[6] = m1.m[6] - m2.m[6];
-        dst.m[7] = m1.m[7] - m2.m[7];
-        dst.m[8] = m1.m[8] - m2.m[8];
-        dst.m[9] = m1.m[9] - m2.m[9];
-        dst.m[10] = m1.m[10] - m2.m[10];
-        dst.m[11] = m1.m[11] - m2.m[11];
-        dst.m[12] = m1.m[12] - m2.m[12];
-        dst.m[13] = m1.m[13] - m2.m[13];
-        dst.m[14] = m1.m[14] - m2.m[14];
-        dst.m[15] = m1.m[15] - m2.m[15];
-    }
-
     template <>
     void Matrix<4, 4, float>::subtract(const Matrix& m1, const Matrix& m2, Matrix& dst)
     {
