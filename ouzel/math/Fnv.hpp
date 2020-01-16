acd6aaebc735dc54e63a891a7d60c8cdbe70a685
Elviss Strazdins <elviss@elviss.lv>
2020-01-16 09:27:38 +0200

    Remake prime and offsetBasis to functions to fix Android build

--- ouzel/math/Fnv.hpp
+++ ouzel/math/Fnv.hpp
@@ -11,20 +11,20 @@ namespace ouzel
     {
         inline namespace detail
         {
-            template <typename T> constexpr T prime = T(0);
-            template <typename T> constexpr T offsetBasis = T(0);
+            template <typename T> T prime();
+            template <typename T> T offsetBasis();
 
-            template <> constexpr uint32_t prime<uint32_t> = 16777619u;
-            template <> constexpr uint32_t offsetBasis<uint32_t> = 2166136261u;
-            template <> constexpr uint64_t prime<uint64_t> = 1099511628211u;
-            template <> constexpr uint64_t offsetBasis<uint64_t> = 14695981039346656037u;
+            template <> constexpr uint32_t prime<uint32_t>() { return 16777619u; }
+            template <> constexpr uint32_t offsetBasis<uint32_t>() { return 2166136261u; }
+                template <> constexpr uint64_t prime<uint64_t>() { return 1099511628211u; }
+                template <> constexpr uint64_t offsetBasis<uint64_t>() { return 14695981039346656037u; }
         }
 
         // Fowler / Noll / Vo (FNV) hash
         template <typename Result, typename Value>
-        constexpr Result hash(const Value value, size_t i = 0, Result result = offsetBasis<Result>) noexcept
+        constexpr Result hash(const Value value, size_t i = 0, Result result = offsetBasis<Result>()) noexcept
         {
-            return (i < sizeof(Value)) ? hash<Result>(value, i + 1, (result * prime<Result>) ^ ((value >> (i * 8)) & 0xFF)) : result;
+            return (i < sizeof(Value)) ? hash<Result>(value, i + 1, (result * prime<Result>()) ^ ((value >> (i * 8)) & 0xFF)) : result;
         }
     } // namespace fnv
 } // namespace ouzel
