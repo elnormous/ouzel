6efd3ef9ce67e61b01b77b30d49d9ae832524d1b
Elviss Strazdins <elviss@elviss.lv>
2020-03-04 18:26:41 +0200

    Move the engine source to the engine directory

--- build/jni/Android.mk
+++ build/jni/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(shell (if [ ! -f $(LOCAL_PATH)/../../ouzel/Config.h ]; then \
-    cp $(LOCAL_PATH)/../../ouzel/DefaultConfig.h $(LOCAL_PATH)/../../ouzel/Config.h; \
+$(shell (if [ ! -f $(LOCAL_PATH)/../../engine/Config.h ]; then \
+    cp $(LOCAL_PATH)/../../engine/DefaultConfig.h $(LOCAL_PATH)/../../engine/Config.h; \
 fi))
 
 LOCAL_MODULE := ouzel
@@ -12,114 +12,114 @@ LOCAL_CFLAGS := -Wall -Wpedantic -Wextra -Wshadow -Wdouble-promotion
 LOCAL_WHOLE_STATIC_LIBRARIES += cpufeatures
 LOCAL_CPPFLAGS += -Wold-style-cast -std=c++14 -fexceptions
 LOCAL_PCH := ../Prefix.pch
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../ouzel \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../engine \
     $(LOCAL_PATH)/../../shaders \
     $(LOCAL_PATH)/../../external/khronos \
 	$(LOCAL_PATH)/../../external/smbPitchShift \
 	$(LOCAL_PATH)/../../external/stb
 
-LOCAL_SRC_FILES := ../../ouzel/assets/BmfLoader.cpp \
-	../../ouzel/assets/Bundle.cpp \
-	../../ouzel/assets/Cache.cpp \
-    ../../ouzel/assets/ColladaLoader.cpp \
-    ../../ouzel/assets/CueLoader.cpp \
-	../../ouzel/assets/GltfLoader.cpp \
-    ../../ouzel/assets/ImageLoader.cpp \
-    ../../ouzel/assets/MtlLoader.cpp \
-    ../../ouzel/assets/ObjLoader.cpp \
-    ../../ouzel/assets/ParticleSystemLoader.cpp \
-    ../../ouzel/assets/SpriteLoader.cpp \
-    ../../ouzel/assets/TtfLoader.cpp \
-    ../../ouzel/assets/VorbisLoader.cpp \
-    ../../ouzel/assets/WaveLoader.cpp \
-    ../../ouzel/audio/empty/EmptyAudioDevice.cpp \
-    ../../ouzel/audio/mixer/Bus.cpp \
-	../../ouzel/audio/mixer/Mixer.cpp \
-    ../../ouzel/audio/opensl/OSLAudioDevice.cpp \
-    ../../ouzel/audio/Audio.cpp \
-    ../../ouzel/audio/AudioDevice.cpp \
-	../../ouzel/audio/Containers.cpp \
-    ../../ouzel/audio/Cue.cpp \
-	../../ouzel/audio/Effect.cpp \
-	../../ouzel/audio/Effects.cpp \
-    ../../ouzel/audio/Listener.cpp \
-	../../ouzel/audio/Mix.cpp \
-    ../../ouzel/audio/Node.cpp \
-    ../../ouzel/audio/Oscillator.cpp \
-    ../../ouzel/audio/PcmClip.cpp \
-	../../ouzel/audio/SilenceSound.cpp \
-    ../../ouzel/audio/Sound.cpp \
-    ../../ouzel/audio/Submix.cpp \
-	../../ouzel/audio/Voice.cpp \
-    ../../ouzel/audio/VorbisClip.cpp \
-    ../../ouzel/core/android/EngineAndroid.cpp \
-    ../../ouzel/core/android/main.cpp \
-    ../../ouzel/core/android/NativeWindowAndroid.cpp \
-	../../ouzel/core/android/SystemAndroid.cpp \
-    ../../ouzel/core/Engine.cpp \
-	../../ouzel/core/NativeWindow.cpp \
-	../../ouzel/core/System.cpp \
-    ../../ouzel/core/Window.cpp \
-    ../../ouzel/events/EventDispatcher.cpp \
-    ../../ouzel/graphics/opengl/android/OGLRenderDeviceAndroid.cpp \
-    ../../ouzel/graphics/opengl/OGLBlendState.cpp \
-    ../../ouzel/graphics/opengl/OGLBuffer.cpp \
-	../../ouzel/graphics/opengl/OGLDepthStencilState.cpp \
-    ../../ouzel/graphics/opengl/OGLRenderDevice.cpp \
-	../../ouzel/graphics/opengl/OGLRenderTarget.cpp \
-    ../../ouzel/graphics/opengl/OGLShader.cpp \
-    ../../ouzel/graphics/opengl/OGLTexture.cpp \
-    ../../ouzel/graphics/BlendState.cpp \
-    ../../ouzel/graphics/Buffer.cpp \
-	../../ouzel/graphics/DepthStencilState.cpp \
-    ../../ouzel/graphics/Renderer.cpp \
-    ../../ouzel/graphics/RenderDevice.cpp \
-	../../ouzel/graphics/RenderTarget.cpp \
-    ../../ouzel/graphics/Shader.cpp \
-    ../../ouzel/graphics/Texture.cpp \
-    ../../ouzel/gui/BMFont.cpp \
-    ../../ouzel/gui/TTFont.cpp \
-    ../../ouzel/gui/Widget.cpp \
-	../../ouzel/gui/Widgets.cpp \
-    ../../ouzel/input/android/GamepadDeviceAndroid.cpp \
-	../../ouzel/input/android/InputSystemAndroid.cpp \
-    ../../ouzel/input/Cursor.cpp \
-    ../../ouzel/input/Gamepad.cpp \
-	../../ouzel/input/GamepadDevice.cpp \
-	../../ouzel/input/InputDevice.cpp \
-    ../../ouzel/input/InputManager.cpp \
-    ../../ouzel/input/InputSystem.cpp \
-	../../ouzel/input/Keyboard.cpp \
-	../../ouzel/input/KeyboardDevice.cpp \
-	../../ouzel/input/Mouse.cpp \
-	../../ouzel/input/MouseDevice.cpp \
-	../../ouzel/input/Touchpad.cpp \
-	../../ouzel/input/TouchpadDevice.cpp \
-    ../../ouzel/localization/Localization.cpp \
-    ../../ouzel/math/MathUtils.cpp \
-    ../../ouzel/math/Matrix.cpp \
-    ../../ouzel/network/Client.cpp \
-    ../../ouzel/network/Network.cpp \
-	../../ouzel/network/Server.cpp \
-    ../../ouzel/scene/Actor.cpp \
-    ../../ouzel/scene/Animator.cpp \
-	../../ouzel/scene/Animators.cpp \
-    ../../ouzel/scene/Camera.cpp \
-    ../../ouzel/scene/Component.cpp \
-    ../../ouzel/scene/Layer.cpp \
-    ../../ouzel/scene/Light.cpp \
-    ../../ouzel/scene/ParticleSystem.cpp \
-    ../../ouzel/scene/Scene.cpp \
-    ../../ouzel/scene/SceneManager.cpp \
-    ../../ouzel/scene/ShapeRenderer.cpp \
-    ../../ouzel/scene/SkinnedMeshRenderer.cpp \
-    ../../ouzel/scene/SpriteRenderer.cpp \
-    ../../ouzel/scene/StaticMeshRenderer.cpp \
-    ../../ouzel/scene/TextRenderer.cpp \
-    ../../ouzel/storage/FileSystem.cpp \
-    ../../ouzel/utils/Log.cpp \
-    ../../ouzel/utils/Obf.cpp \
-    ../../ouzel/utils/Utils.cpp
+LOCAL_SRC_FILES := ../../engine/assets/BmfLoader.cpp \
+	../../engine/assets/Bundle.cpp \
+	../../engine/assets/Cache.cpp \
+    ../../engine/assets/ColladaLoader.cpp \
+    ../../engine/assets/CueLoader.cpp \
+	../../engine/assets/GltfLoader.cpp \
+    ../../engine/assets/ImageLoader.cpp \
+    ../../engine/assets/MtlLoader.cpp \
+    ../../engine/assets/ObjLoader.cpp \
+    ../../engine/assets/ParticleSystemLoader.cpp \
+    ../../engine/assets/SpriteLoader.cpp \
+    ../../engine/assets/TtfLoader.cpp \
+    ../../engine/assets/VorbisLoader.cpp \
+    ../../engine/assets/WaveLoader.cpp \
+    ../../engine/audio/empty/EmptyAudioDevice.cpp \
+    ../../engine/audio/mixer/Bus.cpp \
+	../../engine/audio/mixer/Mixer.cpp \
+    ../../engine/audio/opensl/OSLAudioDevice.cpp \
+    ../../engine/audio/Audio.cpp \
+    ../../engine/audio/AudioDevice.cpp \
+	../../engine/audio/Containers.cpp \
+    ../../engine/audio/Cue.cpp \
+	../../engine/audio/Effect.cpp \
+	../../engine/audio/Effects.cpp \
+    ../../engine/audio/Listener.cpp \
+	../../engine/audio/Mix.cpp \
+    ../../engine/audio/Node.cpp \
+    ../../engine/audio/Oscillator.cpp \
+    ../../engine/audio/PcmClip.cpp \
+	../../engine/audio/SilenceSound.cpp \
+    ../../engine/audio/Sound.cpp \
+    ../../engine/audio/Submix.cpp \
+	../../engine/audio/Voice.cpp \
+    ../../engine/audio/VorbisClip.cpp \
+    ../../engine/core/android/EngineAndroid.cpp \
+    ../../engine/core/android/main.cpp \
+    ../../engine/core/android/NativeWindowAndroid.cpp \
+	../../engine/core/android/SystemAndroid.cpp \
+    ../../engine/core/Engine.cpp \
+	../../engine/core/NativeWindow.cpp \
+	../../engine/core/System.cpp \
+    ../../engine/core/Window.cpp \
+    ../../engine/events/EventDispatcher.cpp \
+    ../../engine/graphics/opengl/android/OGLRenderDeviceAndroid.cpp \
+    ../../engine/graphics/opengl/OGLBlendState.cpp \
+    ../../engine/graphics/opengl/OGLBuffer.cpp \
+	../../engine/graphics/opengl/OGLDepthStencilState.cpp \
+    ../../engine/graphics/opengl/OGLRenderDevice.cpp \
+	../../engine/graphics/opengl/OGLRenderTarget.cpp \
+    ../../engine/graphics/opengl/OGLShader.cpp \
+    ../../engine/graphics/opengl/OGLTexture.cpp \
+    ../../engine/graphics/BlendState.cpp \
+    ../../engine/graphics/Buffer.cpp \
+	../../engine/graphics/DepthStencilState.cpp \
+    ../../engine/graphics/Renderer.cpp \
+    ../../engine/graphics/RenderDevice.cpp \
+	../../engine/graphics/RenderTarget.cpp \
+    ../../engine/graphics/Shader.cpp \
+    ../../engine/graphics/Texture.cpp \
+    ../../engine/gui/BMFont.cpp \
+    ../../engine/gui/TTFont.cpp \
+    ../../engine/gui/Widget.cpp \
+	../../engine/gui/Widgets.cpp \
+    ../../engine/input/android/GamepadDeviceAndroid.cpp \
+	../../engine/input/android/InputSystemAndroid.cpp \
+    ../../engine/input/Cursor.cpp \
+    ../../engine/input/Gamepad.cpp \
+	../../engine/input/GamepadDevice.cpp \
+	../../engine/input/InputDevice.cpp \
+    ../../engine/input/InputManager.cpp \
+    ../../engine/input/InputSystem.cpp \
+	../../engine/input/Keyboard.cpp \
+	../../engine/input/KeyboardDevice.cpp \
+	../../engine/input/Mouse.cpp \
+	../../engine/input/MouseDevice.cpp \
+	../../engine/input/Touchpad.cpp \
+	../../engine/input/TouchpadDevice.cpp \
+    ../../engine/localization/Localization.cpp \
+    ../../engine/math/MathUtils.cpp \
+    ../../engine/math/Matrix.cpp \
+    ../../engine/network/Client.cpp \
+    ../../engine/network/Network.cpp \
+	../../engine/network/Server.cpp \
+    ../../engine/scene/Actor.cpp \
+    ../../engine/scene/Animator.cpp \
+	../../engine/scene/Animators.cpp \
+    ../../engine/scene/Camera.cpp \
+    ../../engine/scene/Component.cpp \
+    ../../engine/scene/Layer.cpp \
+    ../../engine/scene/Light.cpp \
+    ../../engine/scene/ParticleSystem.cpp \
+    ../../engine/scene/Scene.cpp \
+    ../../engine/scene/SceneManager.cpp \
+    ../../engine/scene/ShapeRenderer.cpp \
+    ../../engine/scene/SkinnedMeshRenderer.cpp \
+    ../../engine/scene/SpriteRenderer.cpp \
+    ../../engine/scene/StaticMeshRenderer.cpp \
+    ../../engine/scene/TextRenderer.cpp \
+    ../../engine/storage/FileSystem.cpp \
+    ../../engine/utils/Log.cpp \
+    ../../engine/utils/Obf.cpp \
+    ../../engine/utils/Utils.cpp
 
 include $(BUILD_STATIC_LIBRARY)
 $(call import-module, android/cpufeatures)
