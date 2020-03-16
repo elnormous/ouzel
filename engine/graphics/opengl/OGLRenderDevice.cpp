7979bbfab2078409965da94ea6d897e3753ba96c
Elviss Strazdins <elviss@elviss.lv>
2020-03-16 05:24:32 +0200

    Try to get function pointer from wgl first

--- engine/graphics/opengl/OGLRenderDevice.cpp
+++ engine/graphics/opengl/OGLRenderDevice.cpp
@@ -53,7 +53,8 @@ inline auto getCoreProcAddress(const char* name)
     return reinterpret_cast<T>(glXGetProcAddress(reinterpret_cast<const GLubyte*>(name)));
 #elif OUZEL_OPENGL_INTERFACE_WGL
     static HMODULE module = LoadLibraryW(L"opengl32.dll");
-    return reinterpret_cast<T>(GetProcAddress(module, name));
+    T result = reinterpret_cast<T>(wglGetProcAddress(name));
+    return result ? result : reinterpret_cast<T>(GetProcAddress(module, name));
 #else
     return reinterpret_cast<T>(reinterpret_cast<std::uintptr_t>(dlsym(RTLD_DEFAULT, name)));
 #endif
