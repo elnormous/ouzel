a1c29558dd701d7a9f8899e6e1af7bbeb8430c17
Elviss Strazdins <elviss@elviss.lv>
2020-05-13 05:11:33 +0300

    Use const

--- engine/storage/FileSystem.hpp
+++ engine/storage/FileSystem.hpp
@@ -476,7 +476,7 @@ namespace ouzel
                 WIN32_FILE_ATTRIBUTE_DATA attributes;
                 if (!GetFileAttributesExW(path.getNative().c_str(), GetFileExInfoStandard, &attributes))
                     throw std::system_error(errno, std::system_category(), "Failed to get file attributes");
-                uint64_t fileSize = static_cast<uint64_t>(attributes.nFileSizeHigh) << (sizeof(attributes.nFileSizeHigh) * 8) | attributes.nFileSizeLow;
+                const uint64_t fileSize = static_cast<uint64_t>(attributes.nFileSizeHigh) << (sizeof(attributes.nFileSizeLow) * 8) | attributes.nFileSizeLow;
                 return static_cast<size_t>(fileSize);
 #elif defined(__unix__) || defined(__APPLE__)
                 struct stat s;
