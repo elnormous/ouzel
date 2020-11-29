d458c53fd98dca5ff67e3b6f56c8632be51a8f5e
Elviss Strazdins <elviss@elviss.lv>
2020-11-29 03:20:09 +0200

    Move OpenGL state cache to a separate header file

--- engine/graphics/opengl/OGLRenderDevice.hpp
+++ engine/graphics/opengl/OGLRenderDevice.hpp
@@ -13,7 +13,6 @@
 #include <memory>
 #include <queue>
 #include <system_error>
-#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -33,6 +32,7 @@
 
 #include "../RenderDevice.hpp"
 #include "OGLShader.hpp"
+#include "OGLStateCache.hpp"
 
 namespace ouzel::graphics::opengl
 {
@@ -587,66 +587,6 @@ namespace ouzel::graphics::opengl
 
         bool textureBaseLevelSupported:1;
         bool textureMaxLevelSupported:1;
-
-        struct StateCache final
-        {
-            GLenum frontFace = GL_CCW;
-
-            std::unordered_map<GLenum, std::array<GLuint, 16>> textures{
-#if !OUZEL_OPENGLES
-                {GL_TEXTURE_1D, std::array<GLuint, 16>{}},
-#endif
-                {GL_TEXTURE_2D, std::array<GLuint, 16>{}},
-                {GL_TEXTURE_3D, std::array<GLuint, 16>{}},
-                {GL_TEXTURE_CUBE_MAP, std::array<GLuint, 16>{}}
-            };
-            GLuint programId = 0;
-            GLuint frameBufferId = 0;
-
-            std::unordered_map<GLenum, GLuint> bufferId{
-                {GL_ELEMENT_ARRAY_BUFFER, 0},
-                {GL_ARRAY_BUFFER, 0}
-            };
-
-            bool blendEnabled = false;
-            GLenum blendModeRGB = 0;
-            GLenum blendModeAlpha = 0;
-            GLenum blendSourceFactorRGB = 0;
-            GLenum blendDestFactorRGB = 0;
-            GLenum blendSourceFactorAlpha = 0;
-            GLenum blendDestFactorAlpha = 0;
-            GLboolean redMask = GL_TRUE;
-            GLboolean greenMask = GL_TRUE;
-            GLboolean blueMask = GL_TRUE;
-            GLboolean alphaMask = GL_TRUE;
-
-#if !OUZEL_OPENGLES
-            GLenum polygonFillMode = GL_FILL;
-#endif
-
-            bool scissorTestEnabled = false;
-            GLint scissorX = 0;
-            GLint scissorY = 0;
-            GLsizei scissorWidth = 0;
-            GLsizei scissorHeight = 0;
-
-            bool depthTestEnabled = false;
-            GLboolean depthMask = GL_TRUE;
-            GLenum depthFunc = GL_LESS;
-            bool stencilTestEnabled = false;
-            GLuint stencilMask = 0xFFFFFFFF;
-
-            GLint viewportX = 0;
-            GLint viewportY = 0;
-            GLsizei viewportWidth = 0;
-            GLsizei viewportHeight = 0;
-            bool cullEnabled = false;
-            GLenum cullFace = GL_NONE;
-
-            std::array<GLfloat, 4> clearColor{{0.0F, 0.0F, 0.0F, 0.0F}};
-            GLfloat clearDepth = 1.0F;
-            GLint clearStencil = 0;
-        };
 
         StateCache stateCache;
 
