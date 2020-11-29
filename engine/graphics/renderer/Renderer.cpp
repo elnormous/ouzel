23dfb10acb2a78f56d0332a257770b9ca8b67ae9
Elviss Strazdins <elviss@elviss.lv>
2020-11-29 03:12:32 +0200

    Start implementing the renderer commands

--- engine/graphics/renderer/Renderer.cpp
+++ engine/graphics/renderer/Renderer.cpp
@@ -21,9 +21,44 @@ namespace ouzel::graphics::renderer
 
     void Renderer::render()
     {
+        for (;;)
+        {
+            std::unique_lock lock(commandQueueMutex);
+            if (commandQueue.empty()) break;
+            Command command = std::move(commandQueue.front());
+            commandQueue.pop();
+            lock.unlock();
+
+            switch (command.type)
+            {
+                case Command::Type::resize:
+                {
+                    // TODO: resize the render devices back buffer
+                    break;
+                }
+
+                case Command::Type::initMaterial: break;
+                case Command::Type::setMaterialParameter: break;
+                case Command::Type::initObject: break;
+                case Command::Type::renderObject: break;
+                case Command::Type::setObjectParameters: break;
+                case Command::Type::initCamera: break;
+                case Command::Type::setCameraParameters: break;
+                case Command::Type::initLight: break;
+                case Command::Type::setLightParameters: break;
+                default:
+                    break;
+            }
+        }
+
         // TODO: sort objects by distance
-        // TODO: render shadow map
-        // TODO: render scene
+
+        // render shadow map
+        //for (const auto& light : lights) render light
+
+        // render objects
+        //for (const auto& object : objects) render object
+
         // TODO: post process
     }
 }
