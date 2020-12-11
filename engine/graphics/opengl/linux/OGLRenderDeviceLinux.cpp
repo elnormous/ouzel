5656bd7c4e5fd302932acdbf485922239ad09997
Elviss Strazdins <elviss@elviss.lv>
2020-12-12 01:57:35 +0200

    Remove the EGL_CONTEXT_OPENGL_DEBUG attribute

--- engine/graphics/opengl/linux/OGLRenderDeviceLinux.cpp
+++ engine/graphics/opengl/linux/OGLRenderDeviceLinux.cpp
@@ -128,7 +128,7 @@ namespace ouzel::graphics::opengl::linux
 
             const EGLint contextAttributes[] = {
                 EGL_CONTEXT_CLIENT_VERSION, version,
-                EGL_CONTEXT_OPENGL_DEBUG, settings.debugRenderer ? EGL_TRUE : EGL_FALSE,
+                // EGL_CONTEXT_FLAGS_KHR, settings.debugRenderer ? EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR : 0,
                 EGL_NONE
             };
 
