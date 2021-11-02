27a0fb168228625fbb40f13f19c0d1a04b5ab02a
Elviss Strazdins <elviss@elviss.lv>
2021-11-02 02:16:17 +0200

    Use math constants for conversions to and from radians

--- engine/math/Scalar.hpp
+++ engine/math/Scalar.hpp
@@ -3,6 +3,8 @@
 #ifndef OUZEL_MATH_SCALAR_HPP
 #define OUZEL_MATH_SCALAR_HPP
 
+#include "Constants.hpp"
+
 namespace ouzel::math
 {
     template <typename T>
@@ -19,12 +21,12 @@ namespace ouzel::math
 
     template <typename T> constexpr auto degToRad(const T x) noexcept
     {
-        return x * T(0.01745329251994329576);
+        return x * tau<T> / T(360);
     }
 
     template <typename T> constexpr auto radToDeg(const T x) noexcept
     {
-        return x * T(57.2957795130823208767);
+        return x * T(360) / tau<T>;
     }
 }
 
