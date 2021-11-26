c1c4299fdf63bf6636dd434702671925af8b69e1
Elviss Strazdins <elviss@elviss.lv>
2021-11-26 02:09:32 +0200

    Add wrapper class for NSRunLoop

---
+++ engine/platform/foundation/RunLoop.hpp
@@ -0,0 +1,46 @@
+// Ouzel by Elviss Strazdins
+
+#ifndef OUZEL_PLATFORM_FOUNDATION_RUNLOOP_HPP
+#define OUZEL_PLATFORM_FOUNDATION_RUNLOOP_HPP
+
+#ifdef __OBJC__
+#  import <Foundation/NSRunLoop.h>
+typedef NSRunLoop* NSRunLoopPtr;
+#else
+#  include <objc/NSObjCRuntime.h>
+using NSRunLoopPtr = id;
+#endif
+
+namespace ouzel::platform::foundation
+{
+    class RunLoop final
+    {
+    public:
+        RunLoop() noexcept;
+        ~RunLoop();
+        RunLoop(const RunLoop& other) noexcept;
+        RunLoop(RunLoop&& other) noexcept;
+        RunLoop& operator=(const RunLoop& other) noexcept;
+        RunLoop& operator=(RunLoop&& other) noexcept;
+
+        bool operator==(const RunLoop& other) const noexcept
+        {
+            return runLoop == other.runLoop;
+        }
+
+        bool operator!=(const RunLoop& other) const noexcept
+        {
+            return runLoop != other.runLoop;
+        }
+
+        operator NSRunLoopPtr() const noexcept { return runLoop; }
+
+        void run() const noexcept;
+        void stop() const noexcept;
+
+    private:
+        NSRunLoopPtr runLoop = nil;
+    };
+}
+
+#endif // OUZEL_PLATFORM_FOUNDATION_RUNLOOP_HPP
---
+++ engine/platform/foundation/RunLoop.mm
@@ -0,0 +1,54 @@
+// Ouzel by Elviss Strazdins
+
+#include "RunLoop.hpp"
+
+namespace ouzel::platform::foundation
+{
+    RunLoop::RunLoop() noexcept:
+        runLoop{[[NSRunLoop currentRunLoop] retain]}
+    {
+    }
+
+    RunLoop::~RunLoop()
+    {
+        [runLoop release];
+    }
+
+    RunLoop::RunLoop(const RunLoop& other) noexcept:
+        runLoop{[other.runLoop retain]}
+    {
+    }
+
+    RunLoop::RunLoop(RunLoop&& other) noexcept:
+        runLoop{other.runLoop}
+    {
+        other.runLoop = nil;
+    }
+
+    RunLoop& RunLoop::operator=(const RunLoop& other) noexcept
+    {
+        if (&other == this) return *this;
+        [runLoop release];
+        runLoop = [other.runLoop retain];
+        return *this;
+    }
+
+    RunLoop& RunLoop::operator=(RunLoop&& other) noexcept
+    {
+        if (&other == this) return *this;
+        [runLoop release];
+        runLoop = other.runLoop;
+        other.runLoop = nil;
+        return *this;
+    }
+
+    void RunLoop::run() const noexcept
+    {
+        [runLoop run];
+    }
+
+    void RunLoop::stop() const noexcept
+    {
+        if (runLoop) CFRunLoopStop([runLoop getCFRunLoop]);
+    }
+}
