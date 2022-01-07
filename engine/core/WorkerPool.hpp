33f525b6c891f079d81541d809c5b23348dfa71d
Elviss Strazdins <elviss@elviss.lv>
2022-01-07 05:59:06 +0200

    Implement task scheduling

--- engine/core/WorkerPool.hpp
+++ engine/core/WorkerPool.hpp
@@ -3,6 +3,10 @@
 #ifndef OUZEL_CORE_WORKERPOOL_HPP
 #define OUZEL_CORE_WORKERPOOL_HPP
 
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
 #include <vector>
 #include "../thread/Thread.hpp"
 #include "../utils/Log.hpp"
@@ -20,13 +24,46 @@ namespace ouzel::core
                 workers.emplace_back(&WorkerPool::work, this);
         }
 
+        ~WorkerPool()
+        {
+            running = false;
+            taskQueueCondition.notify_all();
+        }
+
+        void addTask(std::function<void()> task)
+        {
+            std::unique_lock lock{taskQueueMutex};
+            taskQueue.push(std::move(task));
+            lock.unlock();
+
+            taskQueueCondition.notify_one();
+        }
+
     private:
         void work()
         {
             log(Log::Level::info) << "Worker started";
+
+            for (;;)
+            {
+                std::unique_lock lock{taskQueueMutex};
+                taskQueueCondition.wait(lock, [this]() noexcept { return !running || !taskQueue.empty(); });
+                if (!running) break;
+                const auto task = std::move(taskQueue.front());
+                taskQueue.pop();
+                lock.unlock();
+
+                task();
+            }
+            
+            log(Log::Level::info) << "Worker finished";
         }
 
         std::vector<thread::Thread> workers;
+        bool running = true;
+        std::queue<std::function<void()>> taskQueue;
+        std::mutex taskQueueMutex;
+        std::condition_variable taskQueueCondition;
     };
 }
 
