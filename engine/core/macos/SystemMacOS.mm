60b706eb8d358f1a0b2864fc5d0a4398bef2127c
Elviss Strazdins <elviss@elviss.lv>
2022-10-24 03:36:50 +0300

    Localize strings

--- engine/core/macos/SystemMacOS.mm
+++ engine/core/macos/SystemMacOS.mm
@@ -169,9 +169,9 @@ namespace ouzel::core::macos
                                                keyEquivalent:@""];
 
         NSMenu* windowsMenu = [[[NSMenu alloc] initWithTitle:NSLocalizedString(@"Window", nil)] autorelease];
-        [windowsMenu addItemWithTitle:@"Minimize" action:@selector(performMiniaturize:) keyEquivalent:@"m"];
-        [windowsMenu addItemWithTitle:@"Zoom" action:@selector(performZoom:) keyEquivalent:@""];
-        
+        [windowsMenu addItemWithTitle:NSLocalizedString(@"Minimize", nil) action:@selector(performMiniaturize:) keyEquivalent:@"m"];
+        [windowsMenu addItemWithTitle:NSLocalizedString(@"Zoom", nil) action:@selector(performZoom:) keyEquivalent:@""];
+
         windowsItem.submenu = windowsMenu;
         NSApp.windowsMenu = windowsMenu;
 
