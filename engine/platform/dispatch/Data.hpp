6513d3e5c2b13b0a5c84b41fbe3c625ccbf4e27c
Elviss Strazdins <elviss@elviss.lv>
2023-02-20 00:47:10 +0200

    Mark the constructor noexcept

--- engine/platform/dispatch/Data.hpp
+++ engine/platform/dispatch/Data.hpp
@@ -13,7 +13,7 @@ namespace ouzel::platform::dispatch
         explicit Data(const void* buffer,
                       const size_t size,
                       const dispatch_queue_t queue,
-                      const dispatch_block_t destructor):
+                      const dispatch_block_t destructor) noexcept:
             data{dispatch_data_create(buffer, size, queue, destructor)}
         {
         }
